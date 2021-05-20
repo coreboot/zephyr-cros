c4d6f36e3f7db76d07835804521562629e54d378
Jordan Yates <jordan.yates@data61.csiro.au>
2021-05-20 11:12:42 +1000

    lora: sx126x: run interrupts on separate workq
    Instantiate a new workqueue for handling interrupts from the modem.
    Interrupts can trigger significant processing, including power
    transitions on multiple devices. If run on the system workqueue, this
    can cause degraded performance and potentially leads to deadlocks.
    
    Signed-off-by: Jordan Yates <jordan.yates@data61.csiro.au>

--- drivers/lora/sx126x_standalone.c
+++ drivers/lora/sx126x_standalone.c
@@ -14,11 +14,14 @@
 LOG_MODULE_DECLARE(sx126x, CONFIG_LORA_LOG_LEVEL);
 
 static const struct gpio_dt_spec sx126x_gpio_reset = GPIO_DT_SPEC_INST_GET(
-		0, reset_gpios);
+	0, reset_gpios);
 static const struct gpio_dt_spec sx126x_gpio_busy = GPIO_DT_SPEC_INST_GET(
-		0, busy_gpios);
+	0, busy_gpios);
 static const struct gpio_dt_spec sx126x_gpio_dio1 = GPIO_DT_SPEC_INST_GET(
-		0, dio1_gpios);
+	0, dio1_gpios);
+
+K_KERNEL_STACK_DEFINE(sx126x_work_q_stack, CONFIG_LORA_SX126X_WORKQUEUE_STACK_SIZE);
+struct k_work_q sx126x_work_q;
 
 void sx126x_reset(struct sx126x_data *dev_data)
 {
@@ -41,13 +44,13 @@ uint32_t sx126x_get_dio1_pin_state(struct sx126x_data *dev_data)
 void sx126x_dio1_irq_enable(struct sx126x_data *dev_data)
 {
 	gpio_pin_interrupt_configure_dt(&sx126x_gpio_dio1,
-				     GPIO_INT_EDGE_TO_ACTIVE);
+					GPIO_INT_EDGE_TO_ACTIVE);
 }
 
 void sx126x_dio1_irq_disable(struct sx126x_data *dev_data)
 {
 	gpio_pin_interrupt_configure_dt(&sx126x_gpio_dio1,
-				     GPIO_INT_DISABLE);
+					GPIO_INT_DISABLE);
 }
 
 static void sx126x_dio1_irq_callback(const struct device *dev,
@@ -57,13 +60,22 @@ static void sx126x_dio1_irq_callback(const struct device *dev,
 						    dio1_irq_callback);
 
 	if (pins & BIT(sx126x_gpio_dio1.pin)) {
-		k_work_submit(&dev_data->dio1_irq_work);
+		k_work_submit_to_queue(&sx126x_work_q, &dev_data->dio1_irq_work);
 	}
 }
 
 int sx126x_variant_init(const struct device *dev)
 {
 	struct sx126x_data *dev_data = dev->data;
+	const struct k_work_queue_config q_cfg = {
+		.name = "sx1262_q"
+	};
+
+	k_work_queue_start(&sx126x_work_q,
+			   sx126x_work_q_stack,
+			   K_KERNEL_STACK_SIZEOF(sx126x_work_q_stack),
+			   -1,
+			   &q_cfg);
 
 	if (gpio_pin_configure_dt(&sx126x_gpio_reset, GPIO_OUTPUT_ACTIVE) ||
 	    gpio_pin_configure_dt(&sx126x_gpio_busy, GPIO_INPUT) ||
