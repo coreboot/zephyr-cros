8e19724559ee29ff11895e64ff4e73e8da2baad4
Jordan Yates <jordan.yates@data61.csiro.au>
2021-07-15 10:16:45 +1000

    lorawan: dedicated init priority
    Run LoRaWAN initialization at the `POST_KERNEL` level, consistent with
    other device drivers and networking stacks (Bluetooth, etc). Use a
    dedicated Kconfig symbol to allow proper sequencing with the dependant
    LoRa drivers.
    
    Signed-off-by: Jordan Yates <jordan.yates@data61.csiro.au>

--- subsys/lorawan/lorawan.c
+++ subsys/lorawan/lorawan.c
@@ -636,4 +636,4 @@ static int lorawan_init(const struct device *dev)
 
 	return 0;
 }
-SYS_INIT(lorawan_init, APPLICATION, CONFIG_KERNEL_INIT_PRIORITY_DEVICE);
+SYS_INIT(lorawan_init, POST_KERNEL, CONFIG_LORAWAN_INIT_PRIORITY);
