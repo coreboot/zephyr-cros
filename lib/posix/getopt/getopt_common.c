jarz-nordic/zephyr

lib: getopt: rework and extend getopt library
Getopt has been rework in this way that calling it does not require
extra state parameter and its execution is thread safe.
Global parameters describing the state of the getopt function have been
made available to ensure full API compatibility in using this library.
However, referencing these global variables directly is not thread
safe. In order to get the state of the getopt function for the thread
that is currently using it, call: getopt_state_get();

Extended the library with getopt_long and getopt_long_only functions.

Signed-off-by: Jakub Rzeszutko <jakub.rzeszutko@nordicsemi.no>

lib/util/getopt/getopt_common.c
@@ -1,0 +1,62 @@
+/*
+ * Copyright (c) 2021 Nordic Semiconductor ASA
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+#include <zephyr.h>
+#include <shell/shell.h>
+#include "getopt.h"
+
+/* Referring  below variables is not thread safe. They reflects getopt state
+ * only when 1 thread is using getopt.
+ * When more threads are using getopt please call getopt_state_get to know
+ * getopt state for the current thread.
+ */
+int opterr = 1;	/* if error message should be printed */
+int optind = 1;	/* index into parent argv vector */
+int optopt;	/* character checked for validity */
+int optreset;	/* reset getopt */
+char *optarg;	/* argument associated with option */
+
+/* Common state for all threads that did not have own getopt state. */
+static struct getopt_state m_getopt_common_state = {
+	.opterr = 1,
+	.optind = 1,
+	.optopt = 0,
+	.optreset = 0,
+	.optarg = NULL,
+
+	.place = "", /* EMSG */
+
+#if CONFIG_GETOPT_LONG
+	.nonopt_start = -1, /* first non option argument (for permute) */
+	.nonopt_end = -1, /* first option after non options (for permute) */
+#endif
+};
+
+/* This function is not thread safe. All threads using getopt are calling
+ * this function.
+ */
+void z_getopt_global_state_update(struct getopt_state *state)
+{
+	opterr = state->opterr;
+	optind = state->optind;
+	optopt = state->optopt;
+	optreset = state->optreset;
+	optarg = state->optarg;
+}
+
+/* It is internal getopt API function, it shall not be called by the user. */
+struct getopt_state *getopt_state_get(void)
+{
+#if CONFIG_SHELL_GETOPT
+	k_tid_t tid = k_current_get();
+	Z_STRUCT_SECTION_FOREACH(shell, sh) {
+		if (tid == sh->ctx->tid) {
+			return &sh->ctx->getopt_state;
+		}
+	}
+#endif
+	/* If not a shell thread return a common pointer */
+	return &m_getopt_common_state;
+}

samples/subsys/shell/shell_module/src/main.c
@@ -90,7 +90,9 @@ static int cmd_demo_ping(const struct shell *shell, size_t argc, char **argv)
 }
 
 #if defined CONFIG_SHELL_GETOPT
-static int cmd_demo_getopt(const struct shell *shell, size_t argc, char **argv)
+/* Thread save usage */
+static int cmd_demo_getopt_ts(const struct shell *shell, size_t argc,
+			      char **argv)
 {
 	struct getopt_state *state;
 	char *cvalue = NULL;
@@ -98,8 +100,8 @@ static int cmd_demo_getopt(const struct shell *shell, size_t argc, char **argv)
 	int bflag = 0;
 	int c;
 
-	while ((c = shell_getopt(shell, argc, argv, "abhc:")) != -1) {
-		state = shell_getopt_state_get(shell);
+	while ((c = getopt(argc, argv, "abhc:")) != -1) {
+		state = getopt_state_get();
 		switch (c) {
 		case 'a':
 			aflag = 1;
@@ -130,6 +132,55 @@ static int cmd_demo_getopt(const struct shell *shell, size_t argc, char **argv)
 				shell_print(shell,
 					"Unknown option character `\\x%x'.",
 					state->optopt);
+			}
+			return 1;
+		default:
+			break;
+		}
+	}
+
+	shell_print(shell, "aflag = %d, bflag = %d", aflag, bflag);
+	return 0;
+}
+
+static int cmd_demo_getopt(const struct shell *shell, size_t argc,
+			      char **argv)
+{
+	char *cvalue = NULL;
+	int aflag = 0;
+	int bflag = 0;
+	int c;
+
+	while ((c = getopt(argc, argv, "abhc:")) != -1) {
+		switch (c) {
+		case 'a':
+			aflag = 1;
+			break;
+		case 'b':
+			bflag = 1;
+			break;
+		case 'c':
+			cvalue = optarg;
+			break;
+		case 'h':
+			/* When getopt is active shell is not parsing
+			 * command handler to print help message. It must
+			 * be done explicitly.
+			 */
+			shell_help(shell);
+			return SHELL_CMD_HELP_PRINTED;
+		case '?':
+			if (optopt == 'c') {
+				shell_print(shell,
+					"Option -%c requires an argument.",
+					optopt);
+			} else if (isprint(optopt)) {
+				shell_print(shell, "Unknown option `-%c'.",
+					optopt);
+			} else {
+				shell_print(shell,
+					"Unknown option character `\\x%x'.",
+					optopt);
 			}
 			return 1;
 		default:
@@ -242,7 +293,11 @@ SHELL_STATIC_SUBCMD_SET_CREATE(sub_demo,
 	SHELL_CMD(params, NULL, "Print params command.", cmd_demo_params),
 	SHELL_CMD(ping, NULL, "Ping command.", cmd_demo_ping),
 #if defined CONFIG_SHELL_GETOPT
-	SHELL_CMD(getopt, NULL,	"Cammand using getopt, looking for: \"abhc:\".",
+	SHELL_CMD(getopt_thread_safe, NULL,
+		  "Cammand using getopt, looking for: \"abhc:\".",
+		  cmd_demo_getopt_ts),
+	SHELL_CMD(getopt, NULL,	"Cammand using getopt looking for: \"abhc:\".\n"
+		  "This way of using is not thread safe",
 		  cmd_demo_getopt),
 #endif
 	SHELL_SUBCMD_SET_END /* Array terminated. */

coreboot/zephyr-cros

Moved getopt libary from utils to posix.
(cherry picked from commit d4559f53faf6cdcca95046f0dfbb1d5dd29b8d22)
GitOrigin-RevId: d4559f53faf6cdcca95046f0dfbb1d5dd29b8d22
Change-Id: If052d1610cafc5b2b503646d3439703e51709240
Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/zephyr/+/3372552
Tested-by: CopyBot Service Account <copybot.service@gmail.com>
Reviewed-by: Aaron Massey <aaronmassey@google.com>
Commit-Queue: Aaron Massey <aaronmassey@google.com>
Tested-by: Aaron Massey <aaronmassey@google.com>

lib/posix/getopt/getopt_common.c
@@ -1,0 +1,64 @@
+/*
+ * Copyright (c) 2021 Nordic Semiconductor ASA
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+#include <zephyr.h>
+#include <shell/shell.h>
+#include "getopt.h"
+
+/* Referring  below variables is not thread safe. They reflects getopt state
+ * only when 1 thread is using getopt.
+ * When more threads are using getopt please call getopt_state_get to know
+ * getopt state for the current thread.
+ */
+int opterr = 1;	/* if error message should be printed */
+int optind = 1;	/* index into parent argv vector */
+int optopt;	/* character checked for validity */
+int optreset;	/* reset getopt */
+char *optarg;	/* argument associated with option */
+
+/* Common state for all threads that did not have own getopt state. */
+static struct getopt_state m_getopt_common_state = {
+	.opterr = 1,
+	.optind = 1,
+	.optopt = 0,
+	.optreset = 0,
+	.optarg = NULL,
+
+	.place = "", /* EMSG */
+
+#if CONFIG_GETOPT_LONG
+	.nonopt_start = -1, /* first non option argument (for permute) */
+	.nonopt_end = -1, /* first option after non options (for permute) */
+#endif
+};
+
+/* This function is not thread safe. All threads using getopt are calling
+ * this function.
+ */
+void z_getopt_global_state_update(struct getopt_state *state)
+{
+	opterr = state->opterr;
+	optind = state->optind;
+	optopt = state->optopt;
+	optreset = state->optreset;
+	optarg = state->optarg;
+}
+
+/* It is internal getopt API function, it shall not be called by the user. */
+struct getopt_state *getopt_state_get(void)
+{
+#if CONFIG_SHELL_GETOPT
+	k_tid_t tid;
+
+	tid = k_current_get();
+	STRUCT_SECTION_FOREACH(shell, sh) {
+		if (tid == sh->ctx->tid) {
+			return &sh->ctx->getopt;
+		}
+	}
+#endif
+	/* If not a shell thread return a common pointer */
+	return &m_getopt_common_state;
+}

samples/subsys/shell/shell_module/src/main.c
@@ -100,7 +100,9 @@ static int cmd_demo_board(const struct shell *sh, size_t argc, char **argv)
 }
 
 #if defined CONFIG_SHELL_GETOPT
-static int cmd_demo_getopt(const struct shell *shell, size_t argc, char **argv)
+/* Thread save usage */
+static int cmd_demo_getopt_ts(const struct shell *sh, size_t argc,
+			      char **argv)
 {
 	struct getopt_state *state;
 	char *cvalue = NULL;
@@ -108,8 +110,8 @@ static int cmd_demo_getopt(const struct shell *shell, size_t argc, char **argv)
 	int bflag = 0;
 	int c;
 
-	while ((c = shell_getopt(shell, argc, argv, "abhc:")) != -1) {
-		state = shell_getopt_state_get(shell);
+	while ((c = getopt(argc, argv, "abhc:")) != -1) {
+		state = getopt_state_get();
 		switch (c) {
 		case 'a':
 			aflag = 1;
@@ -125,19 +127,19 @@ static int cmd_demo_getopt(const struct shell *shell, size_t argc, char **argv)
 			 * command handler to print help message. It must
 			 * be done explicitly.
 			 */
-			shell_help(shell);
+			shell_help(sh);
 			return SHELL_CMD_HELP_PRINTED;
 		case '?':
 			if (state->optopt == 'c') {
-				shell_print(shell,
+				shell_print(sh,
 					"Option -%c requires an argument.",
 					state->optopt);
 			} else if (isprint(state->optopt)) {
-				shell_print(shell,
+				shell_print(sh,
 					"Unknown option `-%c'.",
 					state->optopt);
 			} else {
-				shell_print(shell,
+				shell_print(sh,
 					"Unknown option character `\\x%x'.",
 					state->optopt);
 			}
@@ -147,7 +149,56 @@ static int cmd_demo_getopt(const struct shell *shell, size_t argc, char **argv)
 		}
 	}
 
-	shell_print(shell, "aflag = %d, bflag = %d", aflag, bflag);
+	shell_print(sh, "aflag = %d, bflag = %d", aflag, bflag);
+	return 0;
+}
+
+static int cmd_demo_getopt(const struct shell *sh, size_t argc,
+			      char **argv)
+{
+	char *cvalue = NULL;
+	int aflag = 0;
+	int bflag = 0;
+	int c;
+
+	while ((c = getopt(argc, argv, "abhc:")) != -1) {
+		switch (c) {
+		case 'a':
+			aflag = 1;
+			break;
+		case 'b':
+			bflag = 1;
+			break;
+		case 'c':
+			cvalue = optarg;
+			break;
+		case 'h':
+			/* When getopt is active shell is not parsing
+			 * command handler to print help message. It must
+			 * be done explicitly.
+			 */
+			shell_help(sh);
+			return SHELL_CMD_HELP_PRINTED;
+		case '?':
+			if (optopt == 'c') {
+				shell_print(sh,
+					"Option -%c requires an argument.",
+					optopt);
+			} else if (isprint(optopt)) {
+				shell_print(sh, "Unknown option `-%c'.",
+					optopt);
+			} else {
+				shell_print(sh,
+					"Unknown option character `\\x%x'.",
+					optopt);
+			}
+			return 1;
+		default:
+			break;
+		}
+	}
+
+	shell_print(sh, "aflag = %d, bflag = %d", aflag, bflag);
 	return 0;
 }
 #endif
@@ -322,8 +373,12 @@ SHELL_STATIC_SUBCMD_SET_CREATE(sub_demo,
 	SHELL_CMD(ping, NULL, "Ping command.", cmd_demo_ping),
 	SHELL_CMD(board, NULL, "Show board name command.", cmd_demo_board),
 #if defined CONFIG_SHELL_GETOPT
-	SHELL_CMD(getopt, NULL,	"Cammand using getopt, looking for: \"abhc:\".",
-		  cmd_demo_getopt),
+	SHELL_CMD(getopt_thread_safe, NULL,
+		  "Cammand using getopt in thread safe way"
+		  " looking for: \"abhc:\".",
+		  cmd_demo_getopt_ts),
+	SHELL_CMD(getopt, NULL, "Cammand using getopt in non thread safe way"
+		  " looking for: \"abhc:\".\n", cmd_demo_getopt),
 #endif
 	SHELL_SUBCMD_SET_END /* Array terminated. */
 );