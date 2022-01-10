zephyrproject-rtos/zephyr

drivers/pcie: Add a function to know MSI/MSI-x support of an endpoint
And since it does yet another round of pcie_get_cap() on PCIE_MSI_CAP_ID
and PCIE_MSIX_CAP_ID, let's factorize that into a utility function and
change the relevant places to use that function instead.

Signed-off-by: Tomasz Bursztyka <tomasz.bursztyka@linux.intel.com>

drivers/pcie/host/msi.c
@@ -9,6 +9,32 @@
 
 /* functions documented in include/drivers/pcie/msi.h */
 
+static uint32_t pcie_msi_x_base(pcie_bdf_t bdf, bool *msi)
+{
+	uint32_t base;
+
+	if (msi != NULL) {
+		*msi = true;
+	}
+
+	base = pcie_get_cap(bdf, PCIE_MSI_CAP_ID);
+
+	if (IS_ENABLED(CONFIG_PCIE_MSI_X)) {
+		uint32_t base_msix;
+
+		base_msix = pcie_get_cap(bdf, PCIE_MSIX_CAP_ID);
+		if (base_msix != 0U) {
+			base = base_msix;
+
+			if (msi != NULL) {
+				*msi = false;
+			}
+		}
+	}
+
+	return base;
+}
+
 #ifdef CONFIG_PCIE_MSI_MULTI_VECTOR
 
 __weak uint8_t arch_pcie_msi_vectors_allocate(unsigned int priority,
@@ -112,23 +138,11 @@ uint8_t pcie_msi_vectors_allocate(pcie_bdf_t bdf,
 				  msi_vector_t *vectors,
 				  uint8_t n_vector)
 {
-	bool msi = true;
-	uint32_t base;
 	uint32_t req_vectors;
+	uint32_t base;
+	bool msi;
 
-	base = pcie_get_cap(bdf, PCIE_MSI_CAP_ID);
-
-	if (IS_ENABLED(CONFIG_PCIE_MSI_X)) {
-		uint32_t base_msix;
-
-		base_msix = pcie_get_cap(bdf, PCIE_MSIX_CAP_ID);
-		if (base_msix != 0U) {
-			base = base_msix;
-		}
-
-		msi = false;
-		base = base_msix;
-	}
+	base = pcie_msi_x_base(bdf, &msi);
 
 	if (IS_ENABLED(CONFIG_PCIE_MSI_X)) {
 		set_msix(vectors, n_vector, !msi);
@@ -161,17 +175,7 @@ bool pcie_msi_vector_connect(pcie_bdf_t bdf,
 {
 	uint32_t base;
 
-	base = pcie_get_cap(bdf, PCIE_MSI_CAP_ID);
-
-	if (IS_ENABLED(CONFIG_PCIE_MSI_X)) {
-		uint32_t base_msix;
-
-		base_msix = pcie_get_cap(bdf, PCIE_MSIX_CAP_ID);
-		if (base_msix != 0U) {
-			base = base_msix;
-		}
-	}
-
+	base = pcie_msi_x_base(bdf, NULL);
 	if (base == 0U) {
 		return false;
 	}
@@ -265,28 +269,16 @@ bool pcie_msi_enable(pcie_bdf_t bdf,
 		     msi_vector_t *vectors,
 		     uint8_t n_vector)
 {
-	bool msi = true;
 	uint32_t base;
+	bool msi;
 
-	base = pcie_get_cap(bdf, PCIE_MSI_CAP_ID);
-
-	if (IS_ENABLED(CONFIG_PCIE_MSI_X)) {
-		uint32_t base_msix;
-
-		base_msix = pcie_get_cap(bdf, PCIE_MSIX_CAP_ID);
-		if ((base_msix != 0U) && (base != 0U)) {
-			disable_msi(bdf, base);
-		}
-
-		msi = false;
-		base = base_msix;
-	}
-
+	base = pcie_msi_x_base(bdf, &msi);
 	if (base == 0U) {
 		return false;
 	}
 
 	if (!msi && IS_ENABLED(CONFIG_PCIE_MSI_X)) {
+		disable_msi(bdf, base);
 		enable_msix(bdf, vectors, n_vector, base);
 	} else {
 		enable_msi(bdf, vectors, n_vector, base);
@@ -295,4 +287,9 @@ bool pcie_msi_enable(pcie_bdf_t bdf,
 	pcie_set_cmd(bdf, PCIE_CONF_CMDSTAT_MASTER, true);
 
 	return true;
+}
+
+bool pcie_is_msi(pcie_bdf_t bdf)
+{
+	return (pcie_msi_x_base(bdf, NULL) != 0);
 }

coreboot/zephyr-cros

(cherry picked from commit fbb2f1511cb7536cbe18ee6b7003d661a92e1bb4)
GitOrigin-RevId: fbb2f1511cb7536cbe18ee6b7003d661a92e1bb4
Change-Id: I10af6a0e204ed88d856774ff569cdfcf3f6ff52c
Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/zephyr/+/3374404
Tested-by: CopyBot Service Account <copybot.service@gmail.com>
Reviewed-by: Keith Short <keithshort@chromium.org>
Tested-by: Keith Short <keithshort@chromium.org>
Commit-Queue: Keith Short <keithshort@chromium.org>

drivers/pcie/host/msi.c
@@ -10,6 +10,32 @@
 
 /* functions documented in include/drivers/pcie/msi.h */
 
+static uint32_t pcie_msi_base(pcie_bdf_t bdf, bool *msi)
+{
+	uint32_t base;
+
+	if (msi != NULL) {
+		*msi = true;
+	}
+
+	base = pcie_get_cap(bdf, PCI_CAP_ID_MSI);
+
+	if (IS_ENABLED(CONFIG_PCIE_MSI_X)) {
+		uint32_t base_msix;
+
+		base_msix = pcie_get_cap(bdf, PCI_CAP_ID_MSIX);
+		if (base_msix != 0U) {
+			base = base_msix;
+
+			if (msi != NULL) {
+				*msi = false;
+			}
+		}
+	}
+
+	return base;
+}
+
 #ifdef CONFIG_PCIE_MSI_MULTI_VECTOR
 
 #include <sys/mem_manage.h>
@@ -115,21 +141,11 @@ uint8_t pcie_msi_vectors_allocate(pcie_bdf_t bdf,
 				  msi_vector_t *vectors,
 				  uint8_t n_vector)
 {
-	bool msi = true;
-	uint32_t base;
 	uint32_t req_vectors;
+	uint32_t base;
+	bool msi;
 
-	base = pcie_get_cap(bdf, PCI_CAP_ID_MSI);
-
-	if (IS_ENABLED(CONFIG_PCIE_MSI_X)) {
-		uint32_t base_msix;
-
-		base_msix = pcie_get_cap(bdf, PCI_CAP_ID_MSIX);
-		if (base_msix != 0U) {
-			msi = false;
-			base = base_msix;
-		}
-	}
+	base = pcie_msi_base(bdf, &msi);
 
 	if (IS_ENABLED(CONFIG_PCIE_MSI_X)) {
 		set_msix(vectors, n_vector, !msi);
@@ -162,17 +178,7 @@ bool pcie_msi_vector_connect(pcie_bdf_t bdf,
 {
 	uint32_t base;
 
-	base = pcie_get_cap(bdf, PCI_CAP_ID_MSI);
-
-	if (IS_ENABLED(CONFIG_PCIE_MSI_X)) {
-		uint32_t base_msix;
-
-		base_msix = pcie_get_cap(bdf, PCI_CAP_ID_MSIX);
-		if (base_msix != 0U) {
-			base = base_msix;
-		}
-	}
-
+	base = pcie_msi_base(bdf, NULL);
 	if (base == 0U) {
 		return false;
 	}
@@ -263,29 +269,16 @@ bool pcie_msi_enable(pcie_bdf_t bdf,
 		     uint8_t n_vector,
 		     unsigned int irq)
 {
-	bool msi = true;
 	uint32_t base;
+	bool msi;
 
-	base = pcie_get_cap(bdf, PCI_CAP_ID_MSI);
-
-	if (IS_ENABLED(CONFIG_PCIE_MSI_X)) {
-		uint32_t base_msix;
-
-		base_msix = pcie_get_cap(bdf, PCI_CAP_ID_MSIX);
-		if ((base_msix != 0U) && (base != 0U)) {
-			disable_msi(bdf, base);
-		}
-		if ((base_msix != 0U)) {
-			msi = false;
-			base = base_msix;
-		}
-	}
-
+	base = pcie_msi_base(bdf, &msi);
 	if (base == 0U) {
 		return false;
 	}
 
 	if (!msi && IS_ENABLED(CONFIG_PCIE_MSI_X)) {
+		disable_msi(bdf, base);
 		enable_msix(bdf, vectors, n_vector, base, irq);
 	} else {
 		enable_msi(bdf, vectors, n_vector, base, irq);
@@ -294,4 +287,9 @@ bool pcie_msi_enable(pcie_bdf_t bdf,
 	pcie_set_cmd(bdf, PCIE_CONF_CMDSTAT_MASTER, true);
 
 	return true;
+}
+
+bool pcie_is_msi(pcie_bdf_t bdf)
+{
+	return (pcie_msi_base(bdf, NULL) != 0);
 }