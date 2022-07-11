aborisovich/zephyr

soc: ace_v1x: pm_state_exit_post_ops implementation
Implementation of the function used during the exit from the lower power
states.

Signed-off-by: Tomasz Leman <tomasz.m.leman@intel.com>

soc/xtensa/intel_adsp/ace_v1x/multiprocessing.c
@@ -190,4 +190,27 @@ __weak void pm_state_set(enum pm_state state, uint8_t substate_id)
 	}
 }
 
+/* Handle SOC specific activity after Low Power Mode Exit */
+__weak void pm_state_exit_post_ops(enum pm_state state, uint8_t substate_id)
+{
+	ARG_UNUSED(substate_id);
+	uint32_t cpu = processor_id_reg();
+
+	switch (state) {
+	case PM_STATE_SOFT_OFF:/* D3 */
+		/* TODO: move clock gating prevent to imr restore vector when it will be ready. */
+		MTL_PWRBOOT.bootctl[cpu].bctl |= MTL_PWRBOOT_BCTL_WAITIPCG;
+		soc_cpus_active[cpu] = true;
+		z_xtensa_cache_flush_inv_all();
+		__fallthrough;
+	case PM_STATE_SUSPEND_TO_IDLE: /* D0ix */
+		__fallthrough;
+	case PM_STATE_RUNTIME_IDLE:/* D0 */
+		break;
+	default:
+		__ASSERT(false, "invalid argument - unsupported power state");
+		break;
+	}
+}
+
 #endif /* CONFIG_PM */

coreboot/zephyr-cros

(cherry picked from commit 0b15a8d3eff6156df9be87311bbbea13e509c5ce)
Original-Signed-off-by: Tomasz Leman <tomasz.m.leman@intel.com>
GitOrigin-RevId: 0b15a8d3eff6156df9be87311bbbea13e509c5ce
Change-Id: Ieb319a8fb0fef9f4ad69882a56f749aa9ee11875
Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/zephyr/+/3753789
Tested-by: Tristan Honscheid <honscheid@google.com>
Commit-Queue: Tristan Honscheid <honscheid@google.com>
Reviewed-by: Tristan Honscheid <honscheid@google.com>
Tested-by: CopyBot Service Account <copybot.service@gmail.com>

soc/xtensa/intel_adsp/ace_v1x/multiprocessing.c
@@ -173,4 +173,27 @@ __weak void pm_state_set(enum pm_state state, uint8_t substate_id)
 	}
 }
 
+/* Handle SOC specific activity after Low Power Mode Exit */
+__weak void pm_state_exit_post_ops(enum pm_state state, uint8_t substate_id)
+{
+	ARG_UNUSED(substate_id);
+	uint32_t cpu = arch_proc_id();
+
+	switch (state) {
+	case PM_STATE_SOFT_OFF:/* D3 */
+		/* TODO: move clock gating prevent to imr restore vector when it will be ready. */
+		MTL_PWRBOOT.bootctl[cpu].bctl |= MTL_PWRBOOT_BCTL_WAITIPCG;
+		soc_cpus_active[cpu] = true;
+		z_xtensa_cache_flush_inv_all();
+		__fallthrough;
+	case PM_STATE_SUSPEND_TO_IDLE: /* D0ix */
+		__fallthrough;
+	case PM_STATE_RUNTIME_IDLE:/* D0 */
+		break;
+	default:
+		__ASSERT(false, "invalid argument - unsupported power state");
+		break;
+	}
+}
+
 #endif /* CONFIG_PM */