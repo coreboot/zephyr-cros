7370d051ed9b414d74ad0cc2479b8b1ea8f12b60
Tokita, Hiroshi <tokita.hiroshi@gmail.com>
2022-08-18 21:50:41 +0900

    x

--- drivers/pio/pio_rpi_pico.c
+++ drivers/pio/pio_rpi_pico.c
@@ -1,1 +1,19 @@
-#error
+#include <zephyr/devicetree.h>
+
+struct {
+} rpi_pico_pio_data;
+
+struct {
+
+} rpi_pico_pio_config;
+
+#define RPI_PICO_PIO_INIT(idx)                                                      \
+        static struct rpi_pico_pio_data rpi_pico_pio_data_##idx = {                     \
+        };                                                                      \
+        static struct rpi_pico_pio_config rpi_pico_pio_config_##idx = {                 \
+        };                                                                      \
+        DEVICE_DT_INST_DEFINE(idx, &rpi_pico_pio_init, NULL, &rpi_pico_pio_data_##idx,  \
+                              &rpi_pico_pio_config_##idx, POST_KERNEL,              \
+                              CONFIG_SPI_INIT_PRIORITY, NULL);
+
+DT_INST_FOREACH_STATUS_OKAY(RPI_PICO_PIO_INIT)
