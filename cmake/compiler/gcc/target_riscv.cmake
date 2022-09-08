a15a31042c80cab2fffb3a1987668a38bd04d9c4
Jeremy Bettis <jbettis@google.com>
2022-09-08 13:19:54 -0600

    CHROMIUM: riscv: Hack riscv arch flags
    When using the coreboot-sdk, don't append zicsr or zifencei to the
    -march flag. Once coreboot-sdk supports isa-spec=20191213 or upgrades
    to gcc 12, then revert this cl.
    
    BRANCH=None
    BUG=b:245409408
    TEST=zmake build -a
    
    Signed-off-by: Jeremy Bettis <jbettis@google.com>
    Change-Id: Ia712f6d97dace15bdb5cb7d36bbf864db046c0d0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/zephyr/+/3884659
    Tested-by: Jeremy Bettis <jbettis@chromium.org>
    Commit-Queue: Jeremy Bettis <jbettis@chromium.org>
    Reviewed-by: Aaron Massey <aaronmassey@google.com>

--- cmake/compiler/gcc/target_riscv.cmake
+++ cmake/compiler/gcc/target_riscv.cmake
@@ -45,12 +45,15 @@ if(CONFIG_RISCV_ISA_EXT_C)
     string(CONCAT riscv_march ${riscv_march} "c")
 endif()
 
-if(CONFIG_RISCV_ISA_EXT_ZICSR)
-    string(CONCAT riscv_march ${riscv_march} "_zicsr")
-endif()
+# TODO(b/245409408): Revert this when coreboot-sdk supports isa-spec=20191213
+if (NOT CMAKE_C_COMPILER STREQUAL "/opt/coreboot-sdk/bin/riscv64-elf-gcc")
+    if(CONFIG_RISCV_ISA_EXT_ZICSR)
+        string(CONCAT riscv_march ${riscv_march} "_zicsr")
+    endif()
 
-if(CONFIG_RISCV_ISA_EXT_ZIFENCEI)
-    string(CONCAT riscv_march ${riscv_march} "_zifencei")
+    if(CONFIG_RISCV_ISA_EXT_ZIFENCEI)
+        string(CONCAT riscv_march ${riscv_march} "_zifencei")
+    endif()
 endif()
 
 if(CONFIG_RISCV_ISA_EXT_ZBA)
