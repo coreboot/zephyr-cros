keith-packard/zephyr

arch/arc: Add -mcpu flag to TOOLCHAIN_LD_FLAGS
Make sure the linker knows what the target is, in case it needs to find
additional target-specific libraries (like picolibc).

Signed-off-by: Keith Packard <keithp@keithp.com>

cmake/compiler/gcc/target_arc.cmake
@@ -9,4 +9,4 @@ if(NOT DEFINED GCC_ARC_TUNED_CPU)
 endif()
 
 list(APPEND TOOLCHAIN_C_FLAGS -mcpu=${GCC_ARC_TUNED_CPU})
-
+list(APPEND TOOLCHAIN_LD_FLAGS  -mcpu=${GCC_M_CPU})

coreboot/zephyr-cros

(cherry picked from commit fc3f1480c550d9e2fa65e52d2df900f0346692fc)
Original-Signed-off-by: Keith Packard <keithp@keithp.com>
GitOrigin-RevId: fc3f1480c550d9e2fa65e52d2df900f0346692fc
Change-Id: I3bce426a8ce5db4af25f4e3fd74ca40c329453d9
Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/zephyr/+/3979614
Reviewed-by: Aaron Massey <aaronmassey@google.com>
Commit-Queue: Aaron Massey <aaronmassey@google.com>
Tested-by: Aaron Massey <aaronmassey@google.com>
Tested-by: CopyBot Service Account <copybot.service@gmail.com>

cmake/compiler/gcc/target_arc.cmake
@@ -9,4 +9,4 @@ if(NOT DEFINED GCC_ARC_TUNED_CPU)
 endif()
 
 list(APPEND TOOLCHAIN_C_FLAGS -mcpu=${GCC_ARC_TUNED_CPU})
-
+list(APPEND TOOLCHAIN_LD_FLAGS -mcpu=${GCC_ARC_TUNED_CPU})