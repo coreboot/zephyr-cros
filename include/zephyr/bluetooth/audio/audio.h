zephyrproject-rtos/zephyr

Bluetooth: Audio: Broadcast source subgroup and BIS codec support
Updates the broadcast source API to create subgroups and
to set BIS specific codec configuration

Signed-off-by: Emil Gydesen <emil.gydesen@nordicsemi.no>

include/zephyr/bluetooth/audio/audio.h
@@ -2144,6 +2144,42 @@ int bt_audio_unicast_group_delete(struct bt_audio_unicast_group *unicast_group);
  * @{
  */
 
+struct bt_audio_broadcast_source_stream_param {
+	/** Audio stream */
+	struct bt_audio_stream *stream;
+
+	/** The number of elements in the %p data array.
+	 *
+	 * The BIS specific data may be omitted and this set to 0.
+	 */
+	size_t data_count;
+
+	/** BIS Codec Specific Configuration */
+	struct bt_codec_data *data;
+};
+
+struct bt_audio_broadcast_source_subgroup_param {
+	/** The number of parameters in @p stream_params */
+	size_t stream_count;
+
+	/** Array of stream parameters */
+	struct bt_audio_broadcast_source_stream_param *stream_params;
+
+	/** Subgroup Codec configuration. */
+	struct bt_codec *codec;
+};
+
+struct bt_audio_broadcast_source_create_param {
+	/** The number of parameters in @p subgroup_params */
+	size_t subgroup_count;
+
+	/** Array of stream parameters */
+	struct bt_audio_broadcast_source_subgroup_param *subgroup_params;
+
+	/** Quality of Service configuration. */
+	struct bt_codec_qos *qos;
+};
+
 /** @brief Create audio broadcast source.
  *
  *  Create a new audio broadcast source with one or more audio streams.
@@ -2155,19 +2191,13 @@ int bt_audio_unicast_group_delete(struct bt_audio_unicast_group *unicast_group);
  *  called and no audio information (BIGInfo) will be visible to scanners
  *  (see bt_le_per_adv_sync_cb).
  *
- *  @param[in]  streams     Array of stream object pointers being used for the
- *                          broadcaster.
- *  @param[in]  num_stream  Number of streams in @p streams.
- *  @param[in]  codec       Codec configuration.
- *  @param[in]  qos         Quality of Service configuration
+ *  @param[in]  param       Pointer to parameters used to create the broadcast
+ *                          source.
  *  @param[out] source      Pointer to the broadcast source created
  *
  *  @return Zero on success or (negative) error code otherwise.
  */
-int bt_audio_broadcast_source_create(struct bt_audio_stream *streams[],
-				     size_t num_stream,
-				     struct bt_codec *codec,
-				     struct bt_codec_qos *qos,
+int bt_audio_broadcast_source_create(struct bt_audio_broadcast_source_create_param *param,
 				     struct bt_audio_broadcast_source **source);
 
 /** @brief Reconfigure audio broadcast source.
@@ -2256,7 +2286,7 @@ int bt_audio_broadcast_source_get_id(const struct bt_audio_broadcast_source *sou
  *
  * @return int		0 if on success, errno on error.
  */
-int bt_audio_broadcast_source_get_base(const struct bt_audio_broadcast_source *source,
+int bt_audio_broadcast_source_get_base(struct bt_audio_broadcast_source *source,
 				       struct net_buf_simple *base_buf);
 
 /** @brief Register Broadcast sink callbacks

subsys/bluetooth/shell/audio.c
@@ -1192,7 +1192,10 @@ static int cmd_select_broadcast_source(const struct shell *sh, size_t argc,
 static int cmd_create_broadcast(const struct shell *sh, size_t argc,
 				char *argv[])
 {
-	struct bt_audio_stream *streams[ARRAY_SIZE(broadcast_source_streams)];
+	struct bt_audio_broadcast_source_stream_param
+		stream_params[ARRAY_SIZE(broadcast_source_streams)];
+	struct bt_audio_broadcast_source_subgroup_param subgroup_param;
+	struct bt_audio_broadcast_source_create_param create_param;
 	struct named_lc3_preset *named_preset;
 	int err;
 
@@ -1212,15 +1215,18 @@ static int cmd_create_broadcast(const struct shell *sh, size_t argc,
 		}
 	}
 
-	(void)memset(streams, 0, sizeof(streams));
-	for (size_t i = 0; i < ARRAY_SIZE(streams); i++) {
-		streams[i] = &broadcast_source_streams[i];
+	(void)memset(stream_params, 0, sizeof(stream_params));
+	for (size_t i = 0; i < ARRAY_SIZE(stream_params); i++) {
+		stream_params[i].stream = &broadcast_source_streams[i];
 	}
+	subgroup_param.stream_count = ARRAY_SIZE(stream_params);
+	subgroup_param.stream_params = stream_params;
+	subgroup_param.codec = &named_preset->preset.codec;
+	create_param.subgroup_count = 1U;
+	create_param.subgroup_params = &subgroup_param;
+	create_param.qos = &named_preset->preset.qos;
 
-	err = bt_audio_broadcast_source_create(streams, ARRAY_SIZE(streams),
-					       &named_preset->preset.codec,
-					       &named_preset->preset.qos,
-					       &default_source);
+	err = bt_audio_broadcast_source_create(&create_param, &default_source);
 	if (err != 0) {
 		shell_error(sh, "Unable to create broadcast source: %d", err);
 		return err;
@@ -1230,7 +1236,7 @@ static int cmd_create_broadcast(const struct shell *sh, size_t argc,
 		    named_preset->name);
 
 	if (default_stream == NULL) {
-		default_stream = streams[0];
+		default_stream = &broadcast_source_streams[0];
 	}
 
 	return 0;

tests/bluetooth/bsim_bt/bsim_test_audio/src/broadcast_source_test.c
@@ -96,13 +96,57 @@ static struct bt_audio_stream_ops stream_ops = {
 	.sent = sent_cb
 };
 
-static int setup_extended_adv(const struct bt_audio_broadcast_source *source,
+static int setup_broadcast_source(struct bt_audio_broadcast_source **source)
+{
+	struct bt_codec_data bis_codec_data = BT_CODEC_DATA(BT_CODEC_CONFIG_LC3_FREQ,
+							    BT_CODEC_CONFIG_LC3_FREQ_16KHZ);
+	struct bt_audio_broadcast_source_stream_param
+		stream_params[ARRAY_SIZE(broadcast_source_streams)];
+	struct bt_audio_broadcast_source_subgroup_param
+		subgroup_params[CONFIG_BT_AUDIO_BROADCAST_SRC_SUBGROUP_COUNT];
+	struct bt_audio_broadcast_source_create_param create_param;
+	int err;
+
+
+	(void)memset(broadcast_source_streams, 0,
+		     sizeof(broadcast_source_streams));
+
+	for (size_t i = 0; i < ARRAY_SIZE(stream_params); i++) {
+		stream_params[i].stream = &broadcast_source_streams[i];
+		bt_audio_stream_cb_register(stream_params[i].stream,
+					    &stream_ops);
+		stream_params[i].data_count = 1U;
+		stream_params[i].data = &bis_codec_data;
+	}
+
+	for (size_t i = 0U; i < ARRAY_SIZE(subgroup_params); i++) {
+		subgroup_params[i].stream_count = 1U;
+		subgroup_params[i].stream_params = &stream_params[i];
+		subgroup_params[i].codec = &preset_16_2_1.codec;
+	}
+
+	create_param.subgroup_count = ARRAY_SIZE(subgroup_params);
+	create_param.subgroup_params = subgroup_params;
+	create_param.qos = &preset_16_2_2.qos;
+
+	printk("Creating broadcast source with %zu subgroups and %zu streams\n",
+	       ARRAY_SIZE(subgroup_params), ARRAY_SIZE(stream_params));
+	err = bt_audio_broadcast_source_create(&create_param, source);
+	if (err != 0) {
+		printk("Unable to create broadcast source: %d\n", err);
+		return err;
+	}
+
+	return 0;
+}
+
+static int setup_extended_adv(struct bt_audio_broadcast_source *source,
 			      struct bt_le_ext_adv **adv)
 {
 	/* Broadcast Audio Streaming Endpoint advertising data */
 	NET_BUF_SIMPLE_DEFINE(ad_buf,
 			      BT_UUID_SIZE_16 + BT_AUDIO_BROADCAST_ID_SIZE);
-	NET_BUF_SIMPLE_DEFINE(base_buf, 64);
+	NET_BUF_SIMPLE_DEFINE(base_buf, 128);
 	struct bt_data ext_ad;
 	struct bt_data per_ad;
 	uint32_t broadcast_id;
@@ -213,21 +257,9 @@ static void test_main(void)
 
 	printk("Bluetooth initialized\n");
 
-	(void)memset(broadcast_source_streams, 0,
-		     sizeof(broadcast_source_streams));
-
-	for (size_t i = 0; i < ARRAY_SIZE(streams); i++) {
-		streams[i] = &broadcast_source_streams[i];
-		bt_audio_stream_cb_register(streams[i], &stream_ops);
-	}
-
-	printk("Creating broadcast source with %zu streams\n", ARRAY_SIZE(streams));
-	err = bt_audio_broadcast_source_create(streams, ARRAY_SIZE(streams),
-					       &preset_16_2_2.codec,
-					       &preset_16_2_2.qos,
-					       &source);
+	err = setup_broadcast_source(&source);
 	if (err != 0) {
-		FAIL("Unable to create broadcast source: %d\n", err);
+		FAIL("Unable to setup broadcast source: %d\n", err);
 		return;
 	}
 
@@ -300,12 +332,9 @@ static void test_main(void)
 
 	/* Recreate broadcast source to verify that it's possible */
 	printk("Recreating broadcast source\n");
-	err = bt_audio_broadcast_source_create(streams, ARRAY_SIZE(streams),
-					       &preset_16_2_1.codec,
-					       &preset_16_2_1.qos,
-					       &source);
+	err = setup_broadcast_source(&source);
 	if (err != 0) {
-		FAIL("Unable to create broadcast source: %d\n", err);
+		FAIL("Unable to setup broadcast source: %d\n", err);
 		return;
 	}
 

coreboot/zephyr-cros

(cherry picked from commit 40e3930ebd43193a9a2090b2242eb35545a37eca)
Original-Signed-off-by: Emil Gydesen <emil.gydesen@nordicsemi.no>
GitOrigin-RevId: 40e3930ebd43193a9a2090b2242eb35545a37eca
Change-Id: I149de7e60527f12651e968ee90d1fbe6a8102d8c
Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/zephyr/+/4055813
Tested-by: CopyBot Service Account <copybot.service@gmail.com>
Reviewed-by: Fabio Baltieri <fabiobaltieri@google.com>
Tested-by: Fabio Baltieri <fabiobaltieri@google.com>
Commit-Queue: Fabio Baltieri <fabiobaltieri@google.com>

include/zephyr/bluetooth/audio/audio.h
@@ -2018,6 +2018,42 @@ int bt_audio_unicast_group_delete(struct bt_audio_unicast_group *unicast_group);
  * @{
  */
 
+struct bt_audio_broadcast_source_stream_param {
+	/** Audio stream */
+	struct bt_audio_stream *stream;
+
+	/** The number of elements in the @p data array.
+	 *
+	 * The BIS specific data may be omitted and this set to 0.
+	 */
+	size_t data_count;
+
+	/** BIS Codec Specific Configuration */
+	struct bt_codec_data *data;
+};
+
+struct bt_audio_broadcast_source_subgroup_param {
+	/** The number of parameters in @p stream_params */
+	size_t params_count;
+
+	/** Array of stream parameters */
+	struct bt_audio_broadcast_source_stream_param *params;
+
+	/** Subgroup Codec configuration. */
+	struct bt_codec *codec;
+};
+
+struct bt_audio_broadcast_source_create_param {
+	/** The number of parameters in @p subgroup_params */
+	size_t params_count;
+
+	/** Array of stream parameters */
+	struct bt_audio_broadcast_source_subgroup_param *params;
+
+	/** Quality of Service configuration. */
+	struct bt_codec_qos *qos;
+};
+
 /** @brief Create audio broadcast source.
  *
  *  Create a new audio broadcast source with one or more audio streams.
@@ -2029,19 +2065,13 @@ int bt_audio_unicast_group_delete(struct bt_audio_unicast_group *unicast_group);
  *  called and no audio information (BIGInfo) will be visible to scanners
  *  (see bt_le_per_adv_sync_cb).
  *
- *  @param[in]  streams     Array of stream object pointers being used for the
- *                          broadcaster.
- *  @param[in]  num_stream  Number of streams in @p streams.
- *  @param[in]  codec       Codec configuration.
- *  @param[in]  qos         Quality of Service configuration
+ *  @param[in]  param       Pointer to parameters used to create the broadcast
+ *                          source.
  *  @param[out] source      Pointer to the broadcast source created
  *
  *  @return Zero on success or (negative) error code otherwise.
  */
-int bt_audio_broadcast_source_create(struct bt_audio_stream *streams[],
-				     size_t num_stream,
-				     struct bt_codec *codec,
-				     struct bt_codec_qos *qos,
+int bt_audio_broadcast_source_create(struct bt_audio_broadcast_source_create_param *param,
 				     struct bt_audio_broadcast_source **source);
 
 /** @brief Reconfigure audio broadcast source.
@@ -2130,7 +2160,7 @@ int bt_audio_broadcast_source_get_id(const struct bt_audio_broadcast_source *sou
  *
  * @return int		0 if on success, errno on error.
  */
-int bt_audio_broadcast_source_get_base(const struct bt_audio_broadcast_source *source,
+int bt_audio_broadcast_source_get_base(struct bt_audio_broadcast_source *source,
 				       struct net_buf_simple *base_buf);
 
 /** @brief Register Broadcast sink callbacks

subsys/bluetooth/shell/audio.c
@@ -1499,7 +1499,10 @@ static int cmd_select_broadcast_source(const struct shell *sh, size_t argc,
 static int cmd_create_broadcast(const struct shell *sh, size_t argc,
 				char *argv[])
 {
-	struct bt_audio_stream *streams[ARRAY_SIZE(broadcast_source_streams)];
+	struct bt_audio_broadcast_source_stream_param
+		stream_params[ARRAY_SIZE(broadcast_source_streams)];
+	struct bt_audio_broadcast_source_subgroup_param subgroup_param;
+	struct bt_audio_broadcast_source_create_param create_param;
 	struct named_lc3_preset *named_preset;
 	int err;
 
@@ -1519,15 +1522,18 @@ static int cmd_create_broadcast(const struct shell *sh, size_t argc,
 		}
 	}
 
-	(void)memset(streams, 0, sizeof(streams));
-	for (size_t i = 0; i < ARRAY_SIZE(streams); i++) {
-		streams[i] = &broadcast_source_streams[i];
+	(void)memset(stream_params, 0, sizeof(stream_params));
+	for (size_t i = 0; i < ARRAY_SIZE(stream_params); i++) {
+		stream_params[i].stream = &broadcast_source_streams[i];
 	}
+	subgroup_param.params_count = ARRAY_SIZE(stream_params);
+	subgroup_param.params = stream_params;
+	subgroup_param.codec = &named_preset->preset.codec;
+	create_param.params_count = 1U;
+	create_param.params = &subgroup_param;
+	create_param.qos = &named_preset->preset.qos;
 
-	err = bt_audio_broadcast_source_create(streams, ARRAY_SIZE(streams),
-					       &named_preset->preset.codec,
-					       &named_preset->preset.qos,
-					       &default_source);
+	err = bt_audio_broadcast_source_create(&create_param, &default_source);
 	if (err != 0) {
 		shell_error(sh, "Unable to create broadcast source: %d", err);
 		return err;
@@ -1537,7 +1543,7 @@ static int cmd_create_broadcast(const struct shell *sh, size_t argc,
 		    named_preset->name);
 
 	if (default_stream == NULL) {
-		default_stream = streams[0];
+		default_stream = &broadcast_source_streams[0];
 	}
 
 	return 0;

tests/bluetooth/bsim_bt/bsim_test_audio/src/broadcast_source_test.c
@@ -96,13 +96,56 @@ static struct bt_audio_stream_ops stream_ops = {
 	.sent = sent_cb
 };
 
-static int setup_extended_adv(const struct bt_audio_broadcast_source *source,
+static int setup_broadcast_source(struct bt_audio_broadcast_source **source)
+{
+	struct bt_codec_data bis_codec_data = BT_CODEC_DATA(BT_CODEC_CONFIG_LC3_FREQ,
+							    BT_CODEC_CONFIG_LC3_FREQ_16KHZ);
+	struct bt_audio_broadcast_source_stream_param
+		stream_params[ARRAY_SIZE(broadcast_source_streams)];
+	struct bt_audio_broadcast_source_subgroup_param
+		subgroup_params[CONFIG_BT_AUDIO_BROADCAST_SRC_SUBGROUP_COUNT];
+	struct bt_audio_broadcast_source_create_param create_param;
+	int err;
+
+	(void)memset(broadcast_source_streams, 0,
+		     sizeof(broadcast_source_streams));
+
+	for (size_t i = 0; i < ARRAY_SIZE(stream_params); i++) {
+		stream_params[i].stream = &broadcast_source_streams[i];
+		bt_audio_stream_cb_register(stream_params[i].stream,
+					    &stream_ops);
+		stream_params[i].data_count = 1U;
+		stream_params[i].data = &bis_codec_data;
+	}
+
+	for (size_t i = 0U; i < ARRAY_SIZE(subgroup_params); i++) {
+		subgroup_params[i].params_count = 1U;
+		subgroup_params[i].params = &stream_params[i];
+		subgroup_params[i].codec = &preset_16_2_1.codec;
+	}
+
+	create_param.params_count = ARRAY_SIZE(subgroup_params);
+	create_param.params = subgroup_params;
+	create_param.qos = &preset_16_2_2.qos;
+
+	printk("Creating broadcast source with %zu subgroups and %zu streams\n",
+	       ARRAY_SIZE(subgroup_params), ARRAY_SIZE(stream_params));
+	err = bt_audio_broadcast_source_create(&create_param, source);
+	if (err != 0) {
+		printk("Unable to create broadcast source: %d\n", err);
+		return err;
+	}
+
+	return 0;
+}
+
+static int setup_extended_adv(struct bt_audio_broadcast_source *source,
 			      struct bt_le_ext_adv **adv)
 {
 	/* Broadcast Audio Streaming Endpoint advertising data */
 	NET_BUF_SIMPLE_DEFINE(ad_buf,
 			      BT_UUID_SIZE_16 + BT_AUDIO_BROADCAST_ID_SIZE);
-	NET_BUF_SIMPLE_DEFINE(base_buf, 64);
+	NET_BUF_SIMPLE_DEFINE(base_buf, 128);
 	struct bt_data ext_ad;
 	struct bt_data per_ad;
 	uint32_t broadcast_id;
@@ -213,21 +256,9 @@ static void test_main(void)
 
 	printk("Bluetooth initialized\n");
 
-	(void)memset(broadcast_source_streams, 0,
-		     sizeof(broadcast_source_streams));
-
-	for (size_t i = 0; i < ARRAY_SIZE(streams); i++) {
-		streams[i] = &broadcast_source_streams[i];
-		bt_audio_stream_cb_register(streams[i], &stream_ops);
-	}
-
-	printk("Creating broadcast source with %zu streams\n", ARRAY_SIZE(streams));
-	err = bt_audio_broadcast_source_create(streams, ARRAY_SIZE(streams),
-					       &preset_16_2_2.codec,
-					       &preset_16_2_2.qos,
-					       &source);
+	err = setup_broadcast_source(&source);
 	if (err != 0) {
-		FAIL("Unable to create broadcast source: %d\n", err);
+		FAIL("Unable to setup broadcast source: %d\n", err);
 		return;
 	}
 
@@ -300,12 +331,9 @@ static void test_main(void)
 
 	/* Recreate broadcast source to verify that it's possible */
 	printk("Recreating broadcast source\n");
-	err = bt_audio_broadcast_source_create(streams, ARRAY_SIZE(streams),
-					       &preset_16_2_1.codec,
-					       &preset_16_2_1.qos,
-					       &source);
+	err = setup_broadcast_source(&source);
 	if (err != 0) {
-		FAIL("Unable to create broadcast source: %d\n", err);
+		FAIL("Unable to setup broadcast source: %d\n", err);
 		return;
 	}
 