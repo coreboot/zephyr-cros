danielhira/zephyr

Bluetooth: Controller: Integrate ISOAL for ISO Broadcast
- Include ISO stream count in ISO streams definitions in
  controller.
- Create ISOAL source on controller when ISO data path is
  setup.
- Send broadcast data buffers transferred from host over
  HCI to ISOAL as ISO SDU fragments.
- Allow bot conn and ISO data to use data_buf_overflow.
- Store ISO interval during BIG create.
- Remove ISO data path for each BIS during BIG terminate
  procedure.
- Set value (temporarily) for ISOAL target_event enabling
  it to Tx data.
- Check status of data fragment sent to ISOAL for memory
  allocation and other errors.
- Destroy ISOAL source when ISO stream released.
- Use ISO Advertising handle, not stream handle to destroy
  ISO data path.
- Remove extra ISOAL sink destroy call when removing ISO
  data path.
- Add FIXME comment as reminder to address LL_ASSERT on
  isoal_status error.

Signed-off-by: Carl Stehle <droid@appception.com>
Signed-off-by: Vinayak Kariappa Chettimada <vich@nordicsemi.no>

subsys/bluetooth/controller/hci/hci_driver.c
@@ -389,55 +389,64 @@ static inline struct net_buf *encode_node(struct node_rx_pdu *node_rx,
 #endif
 #if defined(CONFIG_BT_CTLR_SYNC_ISO) || defined(CONFIG_BT_CTLR_CONN_ISO)
 	case HCI_CLASS_ISO_DATA: {
+		if (false) {
+
 #if defined(CONFIG_BT_CTLR_CONN_ISO)
-		uint8_t handle = node_rx->hdr.handle;
-		struct ll_iso_stream_hdr *hdr = NULL;
-		struct ll_conn_iso_stream *cis = NULL;
-		struct ll_iso_datapath *dp = NULL;
+		} else if (IS_CIS_HANDLE(node_rx->hdr.handle)) {
+			struct ll_conn_iso_stream *cis;
+			uint16_t stream_handle;
 
-		if (IS_CIS_HANDLE(handle)) {
-			cis = ll_conn_iso_stream_get(handle);
-			hdr = &cis->hdr;
-		}
+			stream_handle = LL_CIS_IDX_FROM_HANDLE(node_rx->hdr.handle);
+			cis = ll_conn_iso_stream_get(stream_handle);
+			if (cis && !cis->teardown) {
+				struct ll_iso_stream_hdr *hdr;
+				struct ll_iso_datapath *dp;
 
-		if (cis && !cis->teardown) {
-			dp = hdr->datapath_out;
-		}
+				hdr = &cis->hdr;
+				dp = hdr->datapath_out;
+				if (dp && dp->path_id == BT_HCI_DATAPATH_ID_HCI) {
+					/* If HCI datapath pass to ISO AL here */
+					struct isoal_pdu_rx pckt_meta = {
+						.meta = &node_rx->hdr.rx_iso_meta,
+						.pdu  = (void *)&node_rx->pdu[0],
+					};
 
-		if (dp && dp->path_id == BT_HCI_DATAPATH_ID_HCI) {
-			/* If HCI datapath pass to ISO AL here */
-			struct isoal_pdu_rx pckt_meta = {
-				.meta = &node_rx->hdr.rx_iso_meta,
-				.pdu  = (struct pdu_iso *) &node_rx->pdu[0]
-			};
+					/* Pass the ISO PDU through ISO-AL */
+					isoal_status_t err =
+						isoal_rx_pdu_recombine(dp->sink_hdl, &pckt_meta);
 
-			/* Pass the ISO PDU through ISO-AL */
-			isoal_status_t err =
-				isoal_rx_pdu_recombine(dp->sink_hdl, &pckt_meta);
-
-			LL_ASSERT(err == ISOAL_STATUS_OK); /* TODO handle err */
-		}
+					/* TODO handle err */
+					LL_ASSERT(err == ISOAL_STATUS_OK);
+				}
+			}
 #endif /* CONFIG_BT_CTLR_CONN_ISO */
 
 #if defined(CONFIG_BT_CTLR_SYNC_ISO)
-		const struct lll_sync_iso_stream *stream;
-		struct isoal_pdu_rx isoal_rx;
-		isoal_status_t err;
+		} else if (IS_SYNC_ISO_HANDLE(node_rx->hdr.handle)) {
+			const struct lll_sync_iso_stream *stream;
+			struct isoal_pdu_rx isoal_rx;
+			uint16_t stream_handle;
+			isoal_status_t err;
 
-		stream = ull_sync_iso_stream_get(node_rx->hdr.handle);
+			stream_handle = LL_BIS_SYNC_IDX_FROM_HANDLE(node_rx->hdr.handle);
+			stream = ull_sync_iso_stream_get(stream_handle);
 
-		/* Check validity of the data path sink. FIXME: A channel disconnect race
-		 * may cause ISO data pending without valid data path.
-		 */
-		if (stream && stream->dp) {
-			isoal_rx.meta = &node_rx->hdr.rx_iso_meta;
-			isoal_rx.pdu = (void *)node_rx->pdu;
-			err = isoal_rx_pdu_recombine(stream->dp->sink_hdl, &isoal_rx);
+			/* Check validity of the data path sink. FIXME: A channel disconnect race
+			 * may cause ISO data pending without valid data path.
+			 */
+			if (stream && stream->dp) {
+				isoal_rx.meta = &node_rx->hdr.rx_iso_meta;
+				isoal_rx.pdu = (void *)node_rx->pdu;
+				err = isoal_rx_pdu_recombine(stream->dp->sink_hdl, &isoal_rx);
 
-			LL_ASSERT(err == ISOAL_STATUS_OK ||
-				  err == ISOAL_STATUS_ERR_SDU_ALLOC);
-		}
+				LL_ASSERT(err == ISOAL_STATUS_OK ||
+					  err == ISOAL_STATUS_ERR_SDU_ALLOC);
+			}
 #endif /* CONFIG_BT_CTLR_SYNC_ISO */
+
+		} else {
+			LL_ASSERT(0);
+		}
 
 		node_rx->hdr.next = NULL;
 		ll_iso_rx_mem_release((void **)&node_rx);

coreboot/zephyr-cros

(cherry picked from commit 714f80d2253002dbcf1fea15df6ca95947d32e42)
Original-Signed-off-by: Carl Stehle <droid@appception.com>
Original-Signed-off-by: Vinayak Kariappa Chettimada <vich@nordicsemi.no>
GitOrigin-RevId: 714f80d2253002dbcf1fea15df6ca95947d32e42
Change-Id: Id390746c8b2084b41a68599aa8fe1d06e2071db0
Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/zephyr/+/4201884
Commit-Queue: Fabio Baltieri <fabiobaltieri@google.com>
Tested-by: Fabio Baltieri <fabiobaltieri@google.com>
Reviewed-by: Fabio Baltieri <fabiobaltieri@google.com>
Tested-by: CopyBot Service Account <copybot.service@gmail.com>

subsys/bluetooth/controller/hci/hci_driver.c
@@ -392,55 +392,62 @@ static inline struct net_buf *encode_node(struct node_rx_pdu *node_rx,
 #endif
 #if defined(CONFIG_BT_CTLR_SYNC_ISO) || defined(CONFIG_BT_CTLR_CONN_ISO)
 	case HCI_CLASS_ISO_DATA: {
+		if (false) {
+
 #if defined(CONFIG_BT_CTLR_CONN_ISO)
-		uint8_t handle = node_rx->hdr.handle;
-		struct ll_iso_stream_hdr *hdr = NULL;
-		struct ll_conn_iso_stream *cis = NULL;
-		struct ll_iso_datapath *dp = NULL;
+		} else if (IS_CIS_HANDLE(node_rx->hdr.handle)) {
+			struct ll_conn_iso_stream *cis;
 
-		if (IS_CIS_HANDLE(handle)) {
-			cis = ll_conn_iso_stream_get(handle);
-			hdr = &cis->hdr;
-		}
+			cis = ll_conn_iso_stream_get(node_rx->hdr.handle);
+			if (cis && !cis->teardown) {
+				struct ll_iso_stream_hdr *hdr;
+				struct ll_iso_datapath *dp;
 
-		if (cis && !cis->teardown) {
-			dp = hdr->datapath_out;
-		}
+				hdr = &cis->hdr;
+				dp = hdr->datapath_out;
+				if (dp && dp->path_id == BT_HCI_DATAPATH_ID_HCI) {
+					/* If HCI datapath pass to ISO AL here */
+					struct isoal_pdu_rx pckt_meta = {
+						.meta = &node_rx->hdr.rx_iso_meta,
+						.pdu  = (void *)&node_rx->pdu[0],
+					};
 
-		if (dp && dp->path_id == BT_HCI_DATAPATH_ID_HCI) {
-			/* If HCI datapath pass to ISO AL here */
-			struct isoal_pdu_rx pckt_meta = {
-				.meta = &node_rx->hdr.rx_iso_meta,
-				.pdu  = (struct pdu_iso *) &node_rx->pdu[0]
-			};
+					/* Pass the ISO PDU through ISO-AL */
+					isoal_status_t err =
+						isoal_rx_pdu_recombine(dp->sink_hdl, &pckt_meta);
 
-			/* Pass the ISO PDU through ISO-AL */
-			isoal_status_t err =
-				isoal_rx_pdu_recombine(dp->sink_hdl, &pckt_meta);
-
-			LL_ASSERT(err == ISOAL_STATUS_OK); /* TODO handle err */
-		}
+					/* TODO handle err */
+					LL_ASSERT(err == ISOAL_STATUS_OK);
+				}
+			}
 #endif /* CONFIG_BT_CTLR_CONN_ISO */
 
 #if defined(CONFIG_BT_CTLR_SYNC_ISO)
-		const struct lll_sync_iso_stream *stream;
-		struct isoal_pdu_rx isoal_rx;
-		isoal_status_t err;
+		} else if (IS_SYNC_ISO_HANDLE(node_rx->hdr.handle)) {
+			const struct lll_sync_iso_stream *stream;
+			struct isoal_pdu_rx isoal_rx;
+			uint16_t stream_handle;
+			isoal_status_t err;
 
-		stream = ull_sync_iso_stream_get(node_rx->hdr.handle);
+			stream_handle = LL_BIS_SYNC_IDX_FROM_HANDLE(node_rx->hdr.handle);
+			stream = ull_sync_iso_stream_get(stream_handle);
 
-		/* Check validity of the data path sink. FIXME: A channel disconnect race
-		 * may cause ISO data pending without valid data path.
-		 */
-		if (stream && stream->dp) {
-			isoal_rx.meta = &node_rx->hdr.rx_iso_meta;
-			isoal_rx.pdu = (void *)node_rx->pdu;
-			err = isoal_rx_pdu_recombine(stream->dp->sink_hdl, &isoal_rx);
+			/* Check validity of the data path sink. FIXME: A channel disconnect race
+			 * may cause ISO data pending without valid data path.
+			 */
+			if (stream && stream->dp) {
+				isoal_rx.meta = &node_rx->hdr.rx_iso_meta;
+				isoal_rx.pdu = (void *)node_rx->pdu;
+				err = isoal_rx_pdu_recombine(stream->dp->sink_hdl, &isoal_rx);
 
-			LL_ASSERT(err == ISOAL_STATUS_OK ||
-				  err == ISOAL_STATUS_ERR_SDU_ALLOC);
-		}
+				LL_ASSERT(err == ISOAL_STATUS_OK ||
+					  err == ISOAL_STATUS_ERR_SDU_ALLOC);
+			}
 #endif /* CONFIG_BT_CTLR_SYNC_ISO */
+
+		} else {
+			LL_ASSERT(0);
+		}
 
 		node_rx->hdr.next = NULL;
 		ll_iso_rx_mem_release((void **)&node_rx);