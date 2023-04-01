tmobile/DevEdge-IoTDevKit-ZephyrRTOS

emul: pi3usb9201: Create pi3usb9201 emulator
Create an emulator for the pi3usb9201 USB charging detector.

Signed-off-by: Keith Short <keithshort@google.com>

include/zephyr/drivers/usb/emul_bc12.h
@@ -1,0 +1,72 @@
+/*
+ * Copyright (c) 2022 Google LLC
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+/**
+ * @file
+ * @brief Backend APIs for the BC1.2 emulators.
+ */
+
+#ifndef ZEPHYR_INCLUDE_DRIVERS_USB_EMUL_BC12_H_
+#define ZEPHYR_INCLUDE_DRIVERS_USB_EMUL_BC12_H_
+
+#include <zephyr/drivers/emul.h>
+#include <zephyr/drivers/usb/usb_bc12.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/**
+ * @brief BC1.2 backend emulator APIs
+ * @defgroup b12_emulator_backend BC1.2 backed emulator APIs
+ * @ingroup io_interfaces
+ * @{
+ */
+
+/**
+ * @cond INTERNAL_HIDDEN
+ *
+ * These are for internal use only, so skip these in public documentation.
+ */
+__subsystem struct bc12_emul_driver_api {
+	int (*set_charging_partner)(const struct emul *emul, enum bc12_type partner_type);
+};
+/**
+ * @endcond
+ */
+
+/**
+ * @brief Set the tye charging partner type connected to the BC1.2 device.
+ *
+ * The corresponding BC1.2 emulator updates the vendor specific registers
+ * to simulate connection of the specified charging partner type.  The emulator
+ * also generates an interrupt for processing by the real driver, if supported.
+ *
+ * @param emul Pointer to the emulator structure for the BC1.2 emulator instance.
+ * @param partner_type The simulated partner type.  Set to BC12_TYPE_NONE to
+ * disconnect the charging partner.
+ *
+ * @retval 0 If successful.
+ * @retval -EINVAL if the partner type is not supported.
+ */
+static inline int bc12_emul_set_charging_partner(const struct emul *target,
+						 enum bc12_type partner_type)
+{
+	const struct bc12_emul_driver_api *backend_api =
+		(const struct bc12_emul_driver_api *)target->backend_api;
+
+	return backend_api->set_charging_partner(emul, partner_type);
+}
+
+#ifdef __cplusplus
+}
+#endif
+
+/**
+ * @}
+ */
+
+#endif /* ZEPHYR_INCLUDE_DRIVERS_USB_EMUL_BC12_H_ */

coreboot/zephyr-cros

(cherry picked from commit 9b8c4ee847f4bd8521c0507e77aa862013eb0079)
Original-Signed-off-by: Keith Short <keithshort@google.com>
GitOrigin-RevId: 9b8c4ee847f4bd8521c0507e77aa862013eb0079
Change-Id: I9f2230fc3ea109a39a98e0992a2a32360392c038
Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/zephyr/+/4388701
Reviewed-by: Al Semjonovs <asemjonovs@google.com>
Tested-by: Al Semjonovs <asemjonovs@google.com>
Tested-by: CopyBot Service Account <copybot.service@gmail.com>
Commit-Queue: Al Semjonovs <asemjonovs@google.com>

include/zephyr/drivers/usb/emul_bc12.h
@@ -1,0 +1,72 @@
+/*
+ * Copyright (c) 2022 Google LLC
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+/**
+ * @file
+ * @brief Backend APIs for the BC1.2 emulators.
+ */
+
+#ifndef ZEPHYR_INCLUDE_DRIVERS_USB_EMUL_BC12_H_
+#define ZEPHYR_INCLUDE_DRIVERS_USB_EMUL_BC12_H_
+
+#include <zephyr/drivers/emul.h>
+#include <zephyr/drivers/usb/usb_bc12.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/**
+ * @brief BC1.2 backend emulator APIs
+ * @defgroup b12_emulator_backend BC1.2 backed emulator APIs
+ * @ingroup io_interfaces
+ * @{
+ */
+
+/**
+ * @cond INTERNAL_HIDDEN
+ *
+ * These are for internal use only, so skip these in public documentation.
+ */
+__subsystem struct bc12_emul_driver_api {
+	int (*set_charging_partner)(const struct emul *emul, enum bc12_type partner_type);
+};
+/**
+ * @endcond
+ */
+
+/**
+ * @brief Set the tye charging partner type connected to the BC1.2 device.
+ *
+ * The corresponding BC1.2 emulator updates the vendor specific registers
+ * to simulate connection of the specified charging partner type.  The emulator
+ * also generates an interrupt for processing by the real driver, if supported.
+ *
+ * @param target Pointer to the emulator structure for the BC1.2 emulator instance.
+ * @param partner_type The simulated partner type.  Set to BC12_TYPE_NONE to
+ * disconnect the charging partner.
+ *
+ * @retval 0 If successful.
+ * @retval -EINVAL if the partner type is not supported.
+ */
+static inline int bc12_emul_set_charging_partner(const struct emul *target,
+						 enum bc12_type partner_type)
+{
+	const struct bc12_emul_driver_api *backend_api =
+		(const struct bc12_emul_driver_api *)target->backend_api;
+
+	return backend_api->set_charging_partner(target, partner_type);
+}
+
+#ifdef __cplusplus
+}
+#endif
+
+/**
+ * @}
+ */
+
+#endif /* ZEPHYR_INCLUDE_DRIVERS_USB_EMUL_BC12_H_ */