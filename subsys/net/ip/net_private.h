krish2718/sdk-zephyr

net: wifi_mgmt: Support to provide raw scan data
Support to provide raw beacon and probe response data
to registered application.

Signed-off-by: Ajay Parida <ajay.parida@nordicsemi.no>

subsys/net/ip/net_private.h
@@ -34,6 +34,9 @@ union net_mgmt_events {
 #if defined(CONFIG_NET_IPV6) && defined(CONFIG_NET_IPV6_MLD)
 	struct net_event_ipv6_route ipv6_route;
 #endif /* CONFIG_NET_IPV6 && CONFIG_NET_IPV6_MLD */
+#if defined(CONFIG_WIFI_FEAT_RAW_SCAN_RESULTS)
+	struct wifi_scan_result raw_scan_result;
+#endif /* CONFIG_WIFI_FEAT_RAW_SCAN_RESULTS */
 	char default_event[DEFAULT_NET_EVENT_INFO_SIZE];
 };
 

coreboot/zephyr-cros

(cherry picked from commit e70bb70651dc01c365a1b62d4ca60f87df094899)
Original-Signed-off-by: Ajay Parida <ajay.parida@nordicsemi.no>
GitOrigin-RevId: e70bb70651dc01c365a1b62d4ca60f87df094899
Change-Id: Ibf37664e228f874801c8881b690dbe19a915ec06
Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/zephyr/+/4529899
Tested-by: CopyBot Service Account <copybot.service@gmail.com>
Tested-by: Fabio Baltieri <fabiobaltieri@google.com>
Reviewed-by: Fabio Baltieri <fabiobaltieri@google.com>
Commit-Queue: Fabio Baltieri <fabiobaltieri@google.com>

subsys/net/ip/net_private.h
@@ -30,6 +30,9 @@ union net_mgmt_events {
 #endif /* CONFIG_NET_DHCPV4 */
 #if defined(CONFIG_NET_L2_WIFI_MGMT)
 	struct wifi_scan_result wifi_scan_result;
+#if defined(CONFIG_WIFI_MGMT_RAW_SCAN_RESULTS)
+	struct wifi_raw_scan_result raw_scan_result;
+#endif /* CONFIG_WIFI_MGMT_RAW_SCAN_RESULTS */
 #endif /* CONFIG_NET_L2_WIFI_MGMT */
 #if defined(CONFIG_NET_IPV6) && defined(CONFIG_NET_IPV6_MLD)
 	struct net_event_ipv6_route ipv6_route;