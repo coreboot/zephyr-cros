8a7320de6d5f476a432cf9b6e032c1de8608f223
Jeremy Bettis <jbettis@google.com>
2023-08-09 20:34:48 +0000

    CHROMIUM: Add #line to i/zephyr/drivers/sensor.h
    Work around coreboot GCC preprocessor bug.
    
    BUG=b:272518464
    TEST=gcc -E zephyr/shim/src/fan.c
    
    Change-Id: I70678e810a9de5ff4547fe25affe6efb729248c8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/zephyr/+/4763275
    Commit-Queue: Keith Short <keithshort@chromium.org>
    Auto-Submit: Jeremy Bettis <jbettis@chromium.org>
    Commit-Queue: Jeremy Bettis <jbettis@chromium.org>
    Tested-by: Jeremy Bettis <jbettis@chromium.org>
    Reviewed-by: Keith Short <keithshort@chromium.org>

--- include/zephyr/drivers/sensor.h
+++ include/zephyr/drivers/sensor.h
@@ -5,6 +5,12 @@
  */
 
 /*
+ * TODO(b/272518464): Work around coreboot GCC preprocessor bug.
+ * #line marks the *next* line, so it is off by one.
+ */
+#line 12
+
+/*
  * Copyright (c) 2016 Intel Corporation
  *
  * SPDX-License-Identifier: Apache-2.0
