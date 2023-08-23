krish2718/sdk-zephyr

net: wifi: Add scan extension for limiting scan results
Add scan extension to allow limiting the scan results to a user defined
maximum number. This parameter is intended to return results with the
best RSSI. It cannot be counted upon to limit the scan times since the
Wi-Fi chips might have to scan all the channels to find APs with the
best RSSI values across all channels.

Signed-off-by: Sachin D Kulkarni <sachin.kulkarni@nordicsemi.no>

subsys/net/l2/wifi/wifi_shell.c
@@ -472,6 +472,7 @@ static int wifi_scan_args_to_params(const struct shell *sh,
 					       {"dwell_time_active", required_argument, 0, 'a'},
 					       {"dwell_time_passive", required_argument, 0, 'p'},
 					       {"ssid", required_argument, 0, 's'},
+					       {"max_bss", required_argument, 0, 'm'},
 					       {0, 0, 0, 0}};
 	int opt_index = 0;
 	int val = -1;
@@ -480,7 +481,7 @@ static int wifi_scan_args_to_params(const struct shell *sh,
 				 IS_ENABLED(CONFIG_GETOPT_LONG),
 				 "getopt_long support is mandatory for Wi-Fi shell");
 
-	while ((opt = getopt_long(argc, argv, "t:b:a:p:s:", long_options, &opt_index)) != -1) {
+	while ((opt = getopt_long(argc, argv, "t:b:a:p:s:m:", long_options, &opt_index)) != -1) {
 		state = getopt_state_get();
 		switch (opt) {
 		case 't':
@@ -524,6 +525,16 @@ static int wifi_scan_args_to_params(const struct shell *sh,
 				shell_fprintf(sh, SHELL_ERROR, "Invalid SSID(s)\n");
 				return -ENOEXEC;
 			}
+			break;
+		case 'm':
+			val = atoi(optarg);
+
+			if ((val < 0) || (val > 65535)) {
+				shell_fprintf(sh, SHELL_ERROR, "Invalid max_bss val\n");
+				return -ENOEXEC;
+			}
+
+			params->max_bss_cnt = val;
 			break;
 		case '?':
 			shell_fprintf(sh, SHELL_ERROR, "Invalid option or option usage: %s\n",
@@ -1215,7 +1226,8 @@ SHELL_STATIC_SUBCMD_SET_CREATE(wifi_commands,
 		    "[-b, --bands <Comma separated list of band values (2/5/6)>] : Bands to be scanned where 2: 2.4 GHz, 5: 5 GHz, 6: 6 GHz.\n"
 		    "[-a, --dwell_time_active <val_in_ms>] : Active scan dwell time (in ms) on a channel. Range 5 - 1000 ms.\n"
 		    "[-p, --dwell_time_passive <val_in_ms>] : Passive scan dwell time (in ms) on a channel. Range 10 - 1000 ms.\n"
-		    "[-s, --ssid <Comma separate list of SSIDs>] : SSID list to scan for.",
+		    "[-s, --ssid <Comma separate list of SSIDs>] : SSID list to scan for.\n"
+		    "[-m, --max_bss <val>] : Max BSSIDs to scan for. Range 1 - 65535.",
 		  cmd_wifi_scan),
 	SHELL_CMD(statistics, NULL, "Wi-Fi interface statistics", cmd_wifi_stats),
 	SHELL_CMD(status, NULL, "Status of the Wi-Fi interface", cmd_wifi_status),

coreboot/zephyr-cros

(cherry picked from commit 01e167988ce95f1bb05c27e0c7ba6263ab9e003d)
Original-Signed-off-by: Sachin D Kulkarni <sachin.kulkarni@nordicsemi.no>
GitOrigin-RevId: 01e167988ce95f1bb05c27e0c7ba6263ab9e003d
Change-Id: Ie7d4891e5efdc44a42f70bb8f438498f9e81942a
Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/zephyr/+/4808562
Commit-Queue: Fabio Baltieri <fabiobaltieri@google.com>
Tested-by: ChromeOS Prod (Robot) <chromeos-ci-prod@chromeos-bot.iam.gserviceaccount.com>
Tested-by: Fabio Baltieri <fabiobaltieri@google.com>
Reviewed-by: Fabio Baltieri <fabiobaltieri@google.com>

subsys/net/l2/wifi/wifi_shell.c
@@ -472,11 +472,12 @@ static int wifi_scan_args_to_params(const struct shell *sh,
 					       {"dwell_time_active", required_argument, 0, 'a'},
 					       {"dwell_time_passive", required_argument, 0, 'p'},
 					       {"ssids", required_argument, 0, 's'},
+					       {"max_bss", required_argument, 0, 'm'},
 					       {0, 0, 0, 0}};
 	int opt_index = 0;
 	int val;
 
-	while ((opt = getopt_long(argc, argv, "t:b:a:p:s:", long_options, &opt_index)) != -1) {
+	while ((opt = getopt_long(argc, argv, "t:b:a:p:s:m:", long_options, &opt_index)) != -1) {
 		state = getopt_state_get();
 		switch (opt) {
 		case 't':
@@ -520,6 +521,16 @@ static int wifi_scan_args_to_params(const struct shell *sh,
 				shell_fprintf(sh, SHELL_ERROR, "Invalid SSID(s)\n");
 				return -ENOEXEC;
 			}
+			break;
+		case 'm':
+			val = atoi(optarg);
+
+			if ((val < 0) || (val > 65535)) {
+				shell_fprintf(sh, SHELL_ERROR, "Invalid max_bss val\n");
+				return -ENOEXEC;
+			}
+
+			params->max_bss_cnt = val;
 			break;
 		case '?':
 			shell_fprintf(sh, SHELL_ERROR, "Invalid option or option usage: %s\n",
@@ -1257,7 +1268,8 @@ SHELL_STATIC_SUBCMD_SET_CREATE(wifi_commands,
 		    "[-b, --bands <Comma separated list of band values (2/5/6)>] : Bands to be scanned where 2: 2.4 GHz, 5: 5 GHz, 6: 6 GHz.\n"
 		    "[-a, --dwell_time_active <val_in_ms>] : Active scan dwell time (in ms) on a channel. Range 5 ms to 1000 ms.\n"
 		    "[-p, --dwell_time_passive <val_in_ms>] : Passive scan dwell time (in ms) on a channel. Range 10 ms to 1000 ms.\n"
-		    "[-s, --ssids <Comma separate list of SSIDs>] : SSID list to scan for.",
+		    "[-s, --ssids <Comma separate list of SSIDs>] : SSID list to scan for.\n"
+		    "[-m, --max_bss <val>] : Maximum BSSes to scan for. Range 1 - 65535.",
 		  cmd_wifi_scan),
 	SHELL_CMD(statistics, NULL, "Wi-Fi interface statistics", cmd_wifi_stats),
 	SHELL_CMD(status, NULL, "Status of the Wi-Fi interface", cmd_wifi_status),