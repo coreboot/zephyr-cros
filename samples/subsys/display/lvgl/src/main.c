faxe1008/zephyr

samples: subsys: display: lvgl: Use input-lvgl for touch input
Changes all board specific configurations to use the new
zephyr,input-lvgl-pointer compatible. The kscan_input node in the boards
remains to have downstream backwards compatibility.

Signed-off-by: Fabian Blatz <fabianblatz@gmail.com>

samples/subsys/display/lvgl/src/main.c
@@ -77,7 +77,7 @@ int main(void)
 	}
 #endif
 
-	if (IS_ENABLED(CONFIG_LV_Z_POINTER_KSCAN)) {
+	if (IS_ENABLED(CONFIG_LV_Z_POINTER_KSCAN) || IS_ENABLED(CONFIG_INPUT_LVGL_POINTER)) {
 		lv_obj_t *hello_world_button;
 
 		hello_world_button = lv_btn_create(lv_scr_act());

coreboot/zephyr-cros

zephyr,lvgl-pointer-input compatible. The kscan_input node in the boards
(cherry picked from commit 116ec2c41cac7bb066a89f842e988c0c5590bf2f)
Original-Signed-off-by: Fabian Blatz <fabianblatz@gmail.com>
GitOrigin-RevId: 116ec2c41cac7bb066a89f842e988c0c5590bf2f
Change-Id: I595563caabc1a92ab1747e997f47e358855f6dca
Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/zephyr/+/4822168
Reviewed-by: Yuval Peress <peress@google.com>
Tested-by: ChromeOS Prod (Robot) <chromeos-ci-prod@chromeos-bot.iam.gserviceaccount.com>
Commit-Queue: Yuval Peress <peress@google.com>
Tested-by: Yuval Peress <peress@google.com>

samples/subsys/display/lvgl/src/main.c
@@ -77,7 +77,7 @@ int main(void)
 	}
 #endif
 
-	if (IS_ENABLED(CONFIG_LV_Z_POINTER_KSCAN)) {
+	if (IS_ENABLED(CONFIG_LV_Z_POINTER_KSCAN) || IS_ENABLED(CONFIG_LV_Z_POINTER_INPUT)) {
 		lv_obj_t *hello_world_button;
 
 		hello_world_button = lv_btn_create(lv_scr_act());