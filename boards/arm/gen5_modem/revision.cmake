68a69172bad2c12dfcc6acb5b96bcb1ad1a73cd3
Andriy Gelman <andriy.gelman@gmail.com>
2024-01-27 17:15:55 -0500

    Add Gen5 Modem board (AB/AD revisions)

---
+++ boards/arm/gen5_modem/revision.cmake
@@ -0,0 +1,8 @@
+set(MODEM_REVISIONS "AB" "AD")
+if (NOT DEFINED BOARD_REVISION)
+  set(BOARD_REVISION "AD")
+else()
+  if (NOT BOARD_REVISION IN_LIST MODEM_REVISIONS)
+    message(FATAL_ERROR "${BOARD_REVISION} is not a valid revision for Gen5 modem. Accepted revisions: ${MODEM_REVISIONS}")
+  endif()
+endif()
