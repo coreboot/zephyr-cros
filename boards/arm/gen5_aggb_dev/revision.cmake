2188e4d26f983d9f923656ab5b2336182e4ccdfd
Andriy Gelman <andriy.gelman@gmail.com>
2024-02-06 14:35:39 -0500

    Add Gen5 Aggregator Dev board

---
+++ boards/arm/gen5_aggb_dev/revision.cmake
@@ -0,0 +1,8 @@
+set(AGGB_REVISIONS "AC")
+if (NOT DEFINED BOARD_REVISION)
+  set(BOARD_REVISION "AC")
+else()
+  if (NOT BOARD_REVISION IN_LIST AGGB_REVISIONS)
+    message(FATAL_ERROR "${BOARD_REVISION} is not a valid revision for Gen5 Dev board. Accepted revisions: ${AGGB_REVISIONS}")
+  endif()
+endif()
