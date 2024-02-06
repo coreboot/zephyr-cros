46b500cba91309f3d3e5882582c612f2fb5ba16c
Andriy Gelman <andriy.gelman@gmail.com>
2024-02-06 14:36:14 -0500

    Add Gen5 Aggregator Downhole board

---
+++ boards/arm/gen5_aggb_downhole/revision.cmake
@@ -0,0 +1,8 @@
+set(AGGB_REVISIONS "AB")
+if (NOT DEFINED BOARD_REVISION)
+  set(BOARD_REVISION "AB")
+else()
+  if (NOT BOARD_REVISION IN_LIST AGGB_REVISIONS)
+    message(FATAL_ERROR "${BOARD_REVISION} is not a valid revision for Gen5 Downhole board. Accepted revisions: ${AGGB_REVISIONS}")
+  endif()
+endif()
