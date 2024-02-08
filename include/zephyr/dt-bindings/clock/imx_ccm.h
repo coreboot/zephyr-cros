ashivinay/zephyr

drivers: clock_control: add support for FlexSPI reclock on NXP iMX RT10XX
Add support for reclocking the FlexSPI on NXP iMX RT10XX. This
functionality requires an SOC specific clock function to set
the clock rate, since the FlexSPI must be reset directly
before applying the new clock frequency.

Signed-off-by: Daniel DeGrasse <daniel.degrasse@nxp.com>

include/zephyr/dt-bindings/clock/imx_ccm.h
@@ -55,4 +55,7 @@
 
 #define IMX_CCM_QTMR_CLK		0x0D00UL
 
+#define IMX_CCM_FLEXSPI_CLK		0x0E00UL
+#define IMX_CCM_FLEXSPI2_CLK		0x0E01UL
+
 #endif /* ZEPHYR_INCLUDE_DT_BINDINGS_CLOCK_IMX_CCM_H_ */

include/zephyr/dt-bindings/clock/imx_ccm_rev2.h
@@ -84,5 +84,8 @@
 #define IMX_CCM_SAI3_CLK        0x2002UL
 #define IMX_CCM_SAI4_CLK        0x2003UL
 
+/* FLEXSPI */
+#define IMX_CCM_FLEXSPI_CLK	0x3000UL
+#define IMX_CCM_FLEXSPI2_CLK	0x3001UL
 
 #endif /* ZEPHYR_INCLUDE_DT_BINDINGS_CLOCK_IMX_CCM_REV2_H_ */

coreboot/zephyr-cros

Note that all clock constants are defined in this commit, since the
memc flexspi driver now depends on a clock node being present.
(cherry picked from commit f81113e94811e2e7381f9a6c78dc5f0e29bc6bd7)
Original-Signed-off-by: Daniel DeGrasse <daniel.degrasse@nxp.com>
GitOrigin-RevId: f81113e94811e2e7381f9a6c78dc5f0e29bc6bd7
Change-Id: If115fe5c0846370791c31e52e672cab57ed8da56
Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/zephyr/+/5280054
Tested-by: ChromeOS Prod (Robot) <chromeos-ci-prod@chromeos-bot.iam.gserviceaccount.com>
Tested-by: Al Semjonovs <asemjonovs@google.com>
Reviewed-by: Al Semjonovs <asemjonovs@google.com>
Commit-Queue: Al Semjonovs <asemjonovs@google.com>

include/zephyr/dt-bindings/clock/imx_ccm.h
@@ -58,4 +58,7 @@
 #define IMX_CCM_ENET_CLK		0x0E00UL
 #define IMX_CCM_ENET_PLL		0x0E01UL
 
+#define IMX_CCM_FLEXSPI_CLK		0x0F00UL
+#define IMX_CCM_FLEXSPI2_CLK		0x0F01UL
+
 #endif /* ZEPHYR_INCLUDE_DT_BINDINGS_CLOCK_IMX_CCM_H_ */

include/zephyr/dt-bindings/clock/imx_ccm_rev2.h
@@ -92,5 +92,8 @@
 #define IMX_CCM_ENET_CLK	0x3000UL
 #define IMX_CCM_ENET_PLL	0x3001UL
 
+/* FLEXSPI */
+#define IMX_CCM_FLEXSPI_CLK	0x4000UL
+#define IMX_CCM_FLEXSPI2_CLK	0x4001UL
 
 #endif /* ZEPHYR_INCLUDE_DT_BINDINGS_CLOCK_IMX_CCM_REV2_H_ */