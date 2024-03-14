cd2a48ab5143c5268678cc19de9a703f4e9aed10
Jeremy Bettis <jbettis@google.com>
2024-03-14 14:35:39 -0600

    Fix #line directive
    A new header was added in CL:5351984
    
    BUG=b:272518464
    TEST=zmake build -a
    
    Change-Id: I0600298bbd87467741d5f54153462e6b58bd0c0c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/zephyr/+/5372628
    Commit-Queue: Aaron Massey <aaronmassey@google.com>
    Tested-by: Aaron Massey <aaronmassey@google.com>
    Commit-Queue: Jeremy Bettis <jbettis@chromium.org>
    Auto-Submit: Jeremy Bettis <jbettis@chromium.org>
    Reviewed-by: Aaron Massey <aaronmassey@google.com>
    Tested-by: Jeremy Bettis <jbettis@chromium.org>

--- kernel/init.c
+++ kernel/init.c
@@ -40,7 +40,7 @@
  * TODO(b/272518464): Work around coreboot GCC preprocessor bug.
  * #line marks the *next* line, so it is off by one.
  */
-#line 43
+#line 44
 
 LOG_MODULE_REGISTER(os, CONFIG_KERNEL_LOG_LEVEL);
 
