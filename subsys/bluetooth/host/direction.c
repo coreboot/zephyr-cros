dztang/zephyr

bluetooth: host: fix typo
Utilize a code spell-checking tool to scan for and correct spelling errors
in all files within the subsys/bluetooth/host directory.

Signed-off-by: Pisit Sawangvonganan <pisit@ndrsolution.com>

subsys/bluetooth/host/direction.c
@@ -1144,7 +1144,7 @@ static int bt_df_set_conn_cte_req_enable(struct bt_conn *conn, bool enable,
 	}
 
 	if (!atomic_test_bit(conn->flags, BT_CONN_CTE_RX_PARAMS_SET)) {
-		LOG_ERR("Can't start CTE requres procedure before CTE RX params setup");
+		LOG_ERR("Can't start CTE requires procedure before CTE RX params setup");
 		return -EINVAL;
 	}
 

coreboot/zephyr-cros

(cherry picked from commit 521b9e2c04f7e109b2879126a737990bdc05351d)
Original-Signed-off-by: Pisit Sawangvonganan <pisit@ndrsolution.com>
GitOrigin-RevId: 521b9e2c04f7e109b2879126a737990bdc05351d
Change-Id: I7c249ddb10c0111cb0e1a814fe9bd4468e82a541
Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/zephyr/+/5658659
Commit-Queue: Ting Shen <phoenixshen@chromium.org>
Tested-by: ChromeOS Prod (Robot) <chromeos-ci-prod@chromeos-bot.iam.gserviceaccount.com>
Reviewed-by: Ting Shen <phoenixshen@chromium.org>

subsys/bluetooth/host/direction.c
@@ -1144,7 +1144,7 @@ static int bt_df_set_conn_cte_req_enable(struct bt_conn *conn, bool enable,
 	}
 
 	if (!atomic_test_bit(conn->flags, BT_CONN_CTE_RX_PARAMS_SET)) {
-		LOG_ERR("Can't start CTE requres procedure before CTE RX params setup");
+		LOG_ERR("Can't start CTE request procedure before CTE RX params setup");
 		return -EINVAL;
 	}
 