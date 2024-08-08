zephyrproject-rtos/zephyr

drivers: flash: reduce redundancy in RDP implementation on STM32
Reduce the redundancy in the readout protection implementation
on STM32 MCUs.

Signed-off-by: Benedikt Schmidt <benedikt.schmidt@embedded-solutions.at>

drivers/flash/flash_stm32_ex_op.c
@@ -8,6 +8,7 @@
 #include <zephyr/device.h>
 #include <zephyr/drivers/flash/stm32_flash_api_extensions.h>
 #include <zephyr/kernel.h>
+#include <zephyr/logging/log.h>
 
 #ifdef CONFIG_USERSPACE
 #include <zephyr/syscall.h>
@@ -16,6 +17,8 @@
 
 #include <soc.h>
 #include "flash_stm32.h"
+
+LOG_MODULE_REGISTER(flash_stm32_ex_op, CONFIG_FLASH_LOG_LEVEL);
 
 #if defined(CONFIG_FLASH_STM32_WRITE_PROTECT)
 int flash_stm32_ex_op_sector_wp(const struct device *dev, const uintptr_t in,
@@ -85,6 +88,71 @@ int flash_stm32_ex_op_sector_wp(const struct device *dev, const uintptr_t in,
 #endif /* CONFIG_FLASH_STM32_WRITE_PROTECT */
 
 #if defined(CONFIG_FLASH_STM32_READOUT_PROTECTION)
+int flash_stm32_ex_op_update_rdp(const struct device *dev, bool enable,
+			   bool permanent)
+{
+	uint8_t current_level, target_level;
+
+	current_level = flash_stm32_get_rdp_level(dev);
+	target_level = current_level;
+
+	/*
+	 * 0xAA = RDP level 0 (no protection)
+	 * 0xCC = RDP level 2 (permanent protection)
+	 * others = RDP level 1 (protection active)
+	 */
+	switch (current_level) {
+	case FLASH_STM32_RDP2:
+		if (!enable || !permanent) {
+			LOG_ERR("RDP level 2 is permanent and can't be changed!");
+			return -ENOTSUP;
+		}
+		break;
+	case FLASH_STM32_RDP0:
+		if (enable) {
+			target_level = FLASH_STM32_RDP1;
+			if (permanent) {
+#if defined(CONFIG_FLASH_STM32_READOUT_PROTECTION_PERMANENT_ALLOW)
+				target_level = FLASH_STM32_RDP2;
+#else
+				LOG_ERR("Permanent readout protection (RDP "
+					"level 0 -> 2) not allowed");
+				return -ENOTSUP;
+#endif
+			}
+		}
+		break;
+	default: /* FLASH_STM32_RDP1 */
+		if (enable && permanent) {
+#if defined(CONFIG_FLASH_STM32_READOUT_PROTECTION_PERMANENT_ALLOW)
+			target_level = FLASH_STM32_RDP2;
+#else
+			LOG_ERR("Permanent readout protection (RDP "
+				"level 1 -> 2) not allowed");
+			return -ENOTSUP;
+#endif
+		}
+		if (!enable) {
+#if defined(CONFIG_FLASH_STM32_READOUT_PROTECTION_DISABLE_ALLOW)
+			target_level = FLASH_STM32_RDP0;
+#else
+			LOG_ERR("Disabling readout protection (RDP "
+				"level 1 -> 0) not allowed");
+			return -EACCES;
+#endif
+		}
+	}
+
+	/* Update RDP level if needed */
+	if (current_level != target_level) {
+		LOG_INF("RDP changed from 0x%02x to 0x%02x", current_level,
+			target_level);
+
+		flash_stm32_set_rdp_level(dev, target_level);
+	}
+	return 0;
+}
+
 int flash_stm32_ex_op_rdp(const struct device *dev, const uintptr_t in,
 			  void *out)
 {
@@ -92,6 +160,7 @@ int flash_stm32_ex_op_rdp(const struct device *dev, const uintptr_t in,
 		(const struct flash_stm32_ex_op_rdp *)in;
 	struct flash_stm32_ex_op_rdp *result =
 		(struct flash_stm32_ex_op_rdp *)out;
+	uint8_t current_level;
 
 #ifdef CONFIG_USERSPACE
 	struct flash_stm32_ex_op_rdp copy;
@@ -108,7 +177,7 @@ int flash_stm32_ex_op_rdp(const struct device *dev, const uintptr_t in,
 #endif
 		rc = flash_stm32_option_bytes_lock(dev, false);
 		if (rc == 0) {
-			rc = flash_stm32_update_rdp(dev, request->enable,
+			rc = flash_stm32_ex_op_update_rdp(dev, request->enable,
 						    request->permanent);
 		}
 
@@ -124,10 +193,26 @@ int flash_stm32_ex_op_rdp(const struct device *dev, const uintptr_t in,
 			result = &copy;
 		}
 #endif
-		rc2 = flash_stm32_get_rdp(dev, &result->enable,
-					  &result->permanent);
-		if (!rc) {
-			rc = rc2;
+
+		current_level = flash_stm32_get_rdp_level(dev);
+
+		/*
+		 * 0xAA = RDP level 0 (no protection)
+		 * 0xCC = RDP level 2 (permanent protection)
+		 * others = RDP level 1 (protection active)
+		 */
+		switch (current_level) {
+		case FLASH_STM32_RDP2:
+			result->enable = true;
+			result->permanent = true;
+			break;
+		case FLASH_STM32_RDP0:
+			result->enable = false;
+			result->permanent = false;
+			break;
+		default: /* FLASH_STM32_RDP1 */
+			result->enable = true;
+			result->permanent = false;
 		}
 
 #ifdef CONFIG_USERSPACE

coreboot/zephyr-cros

(cherry picked from commit db2261b6f5273db529433d032e75cce3f4d9d0f3)
Original-Signed-off-by: Benedikt Schmidt <benedikt.schmidt@embedded-solutions.at>
GitOrigin-RevId: db2261b6f5273db529433d032e75cce3f4d9d0f3
Cr-Build-Id: 8740178774340358353
Cr-Build-Url: https://cr-buildbucket.appspot.com/build/8740178774340358353
Copybot-Job-Name: zephyr-main-copybot-downstream
Change-Id: I70b8506e0ee814adde0250d67bbb6b88d5b95ba5
Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/zephyr/+/5773273
Reviewed-by: Jeremy Bettis <jbettis@chromium.org>
Tested-by: Jeremy Bettis <jbettis@chromium.org>
Commit-Queue: Jeremy Bettis <jbettis@chromium.org>
Tested-by: ChromeOS Prod (Robot) <chromeos-ci-prod@chromeos-bot.iam.gserviceaccount.com>

drivers/flash/flash_stm32_ex_op.c
@@ -8,6 +8,7 @@
 #include <zephyr/device.h>
 #include <zephyr/drivers/flash/stm32_flash_api_extensions.h>
 #include <zephyr/kernel.h>
+#include <zephyr/logging/log.h>
 
 #ifdef CONFIG_USERSPACE
 #include <zephyr/syscall.h>
@@ -16,6 +17,8 @@
 
 #include <soc.h>
 #include "flash_stm32.h"
+
+LOG_MODULE_REGISTER(flash_stm32_ex_op, CONFIG_FLASH_LOG_LEVEL);
 
 #if defined(CONFIG_FLASH_STM32_WRITE_PROTECT)
 int flash_stm32_ex_op_sector_wp(const struct device *dev, const uintptr_t in,
@@ -85,6 +88,71 @@ int flash_stm32_ex_op_sector_wp(const struct device *dev, const uintptr_t in,
 #endif /* CONFIG_FLASH_STM32_WRITE_PROTECT */
 
 #if defined(CONFIG_FLASH_STM32_READOUT_PROTECTION)
+int flash_stm32_ex_op_update_rdp(const struct device *dev, bool enable,
+			   bool permanent)
+{
+	uint8_t current_level, target_level;
+
+	current_level = flash_stm32_get_rdp_level(dev);
+	target_level = current_level;
+
+	/*
+	 * 0xAA = RDP level 0 (no protection)
+	 * 0xCC = RDP level 2 (permanent protection)
+	 * others = RDP level 1 (protection active)
+	 */
+	switch (current_level) {
+	case FLASH_STM32_RDP2:
+		if (!enable || !permanent) {
+			LOG_DBG("RDP level 2 is permanent and can't be changed!");
+			return -ENOTSUP;
+		}
+		break;
+	case FLASH_STM32_RDP0:
+		if (enable) {
+			target_level = FLASH_STM32_RDP1;
+			if (permanent) {
+#if defined(CONFIG_FLASH_STM32_READOUT_PROTECTION_PERMANENT_ALLOW)
+				target_level = FLASH_STM32_RDP2;
+#else
+				LOG_DBG("Permanent readout protection (RDP "
+					"level 0 -> 2) not allowed");
+				return -ENOTSUP;
+#endif
+			}
+		}
+		break;
+	default: /* FLASH_STM32_RDP1 */
+		if (enable && permanent) {
+#if defined(CONFIG_FLASH_STM32_READOUT_PROTECTION_PERMANENT_ALLOW)
+			target_level = FLASH_STM32_RDP2;
+#else
+			LOG_DBG("Permanent readout protection (RDP "
+				"level 1 -> 2) not allowed");
+			return -ENOTSUP;
+#endif
+		}
+		if (!enable) {
+#if defined(CONFIG_FLASH_STM32_READOUT_PROTECTION_DISABLE_ALLOW)
+			target_level = FLASH_STM32_RDP0;
+#else
+			LOG_DBG("Disabling readout protection (RDP "
+				"level 1 -> 0) not allowed");
+			return -EACCES;
+#endif
+		}
+	}
+
+	/* Update RDP level if needed */
+	if (current_level != target_level) {
+		LOG_INF("RDP changed from 0x%02x to 0x%02x", current_level,
+			target_level);
+
+		flash_stm32_set_rdp_level(dev, target_level);
+	}
+	return 0;
+}
+
 int flash_stm32_ex_op_rdp(const struct device *dev, const uintptr_t in,
 			  void *out)
 {
@@ -92,6 +160,7 @@ int flash_stm32_ex_op_rdp(const struct device *dev, const uintptr_t in,
 		(const struct flash_stm32_ex_op_rdp *)in;
 	struct flash_stm32_ex_op_rdp *result =
 		(struct flash_stm32_ex_op_rdp *)out;
+	uint8_t current_level;
 
 #ifdef CONFIG_USERSPACE
 	struct flash_stm32_ex_op_rdp copy;
@@ -108,7 +177,7 @@ int flash_stm32_ex_op_rdp(const struct device *dev, const uintptr_t in,
 #endif
 		rc = flash_stm32_option_bytes_lock(dev, false);
 		if (rc == 0) {
-			rc = flash_stm32_update_rdp(dev, request->enable,
+			rc = flash_stm32_ex_op_update_rdp(dev, request->enable,
 						    request->permanent);
 		}
 
@@ -124,10 +193,26 @@ int flash_stm32_ex_op_rdp(const struct device *dev, const uintptr_t in,
 			result = &copy;
 		}
 #endif
-		rc2 = flash_stm32_get_rdp(dev, &result->enable,
-					  &result->permanent);
-		if (!rc) {
-			rc = rc2;
+
+		current_level = flash_stm32_get_rdp_level(dev);
+
+		/*
+		 * 0xAA = RDP level 0 (no protection)
+		 * 0xCC = RDP level 2 (permanent protection)
+		 * others = RDP level 1 (protection active)
+		 */
+		switch (current_level) {
+		case FLASH_STM32_RDP2:
+			result->enable = true;
+			result->permanent = true;
+			break;
+		case FLASH_STM32_RDP0:
+			result->enable = false;
+			result->permanent = false;
+			break;
+		default: /* FLASH_STM32_RDP1 */
+			result->enable = true;
+			result->permanent = false;
 		}
 
 #ifdef CONFIG_USERSPACE