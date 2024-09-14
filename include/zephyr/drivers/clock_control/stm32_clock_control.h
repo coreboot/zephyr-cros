HoZHel/zephyr_ST

drivers: clock: add STM32WB0 clock control
This commit adds clock control driver for STM32WB0 series.
All clocks should be supported.

Signed-off-by: Mathieu Choplain <mathieu.choplain@st.com>

include/zephyr/drivers/clock_control/stm32_clock_control.h
@@ -38,6 +38,8 @@
 #include <zephyr/dt-bindings/clock/stm32l4_clock.h>
 #elif defined(CONFIG_SOC_SERIES_STM32WBX)
 #include <zephyr/dt-bindings/clock/stm32wb_clock.h>
+#elif defined(CONFIG_SOC_SERIES_STM32WB0)
+#include <zephyr/dt-bindings/clock/stm32wb0_clock.h>
 #elif defined(CONFIG_SOC_SERIES_STM32WLX)
 #include <zephyr/dt-bindings/clock/stm32wl_clock.h>
 #elif defined(CONFIG_SOC_SERIES_STM32H5X)
@@ -495,5 +497,26 @@ struct stm32_pclken {
  */
 void stm32_hse_css_callback(void);
 #endif
+
+#ifdef CONFIG_SOC_SERIES_STM32WB0
+/**
+ * @internal
+ * @brief Type definition for LSI frequency update callbacks
+ */
+typedef void (*lsi_update_cb_t)(uint32_t new_lsi_frequency);
+
+/**
+ * @internal
+ * @brief Registers a callback to invoke after each runtime measure and
+ * update of the LSI frequency is completed.
+ *
+ * @param cb		Callback to invoke
+ * @return 0		Registration successful
+ * @return ENOMEM	Too many callbacks registered
+ *
+ * @note Callbacks are NEVER invoked if runtime LSI measurement is disabled
+ */
+int stm32wb0_register_lsi_update_callback(lsi_update_cb_t cb);
+#endif /* CONFIG_SOC_SERIES_STM32WB0 */
 
 #endif /* ZEPHYR_INCLUDE_DRIVERS_CLOCK_CONTROL_STM32_CLOCK_CONTROL_H_ */

coreboot/zephyr-cros

Add control driver for STM32WB0 series, with support for all clock sources.
(cherry picked from commit 20c45fe10a15d42baf89c25d41cfa25515f54ef4)
Original-Signed-off-by: Mathieu Choplain <mathieu.choplain@st.com>
GitOrigin-RevId: 20c45fe10a15d42baf89c25d41cfa25515f54ef4
Cr-Build-Id: 8736826685699823921
Cr-Build-Url: https://cr-buildbucket.appspot.com/build/8736826685699823921
Copybot-Job-Name: zephyr-main-copybot-downstream
Change-Id: I2dd1b24ab25b7f7a356a359d77cfdedc1fd44ca4
Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/zephyr/+/5857566
Reviewed-by: Keith Short <keithshort@chromium.org>
Commit-Queue: Keith Short <keithshort@chromium.org>
Tested-by: Keith Short <keithshort@chromium.org>
Tested-by: ChromeOS Prod (Robot) <chromeos-ci-prod@chromeos-bot.iam.gserviceaccount.com>

include/zephyr/drivers/clock_control/stm32_clock_control.h
@@ -38,6 +38,8 @@
 #include <zephyr/dt-bindings/clock/stm32l4_clock.h>
 #elif defined(CONFIG_SOC_SERIES_STM32WBX)
 #include <zephyr/dt-bindings/clock/stm32wb_clock.h>
+#elif defined(CONFIG_SOC_SERIES_STM32WB0X)
+#include <zephyr/dt-bindings/clock/stm32wb0_clock.h>
 #elif defined(CONFIG_SOC_SERIES_STM32WLX)
 #include <zephyr/dt-bindings/clock/stm32wl_clock.h>
 #elif defined(CONFIG_SOC_SERIES_STM32H5X)
@@ -498,5 +500,26 @@ struct stm32_pclken {
  */
 void stm32_hse_css_callback(void);
 #endif
+
+#ifdef CONFIG_SOC_SERIES_STM32WB0X
+/**
+ * @internal
+ * @brief Type definition for LSI frequency update callbacks
+ */
+typedef void (*lsi_update_cb_t)(uint32_t new_lsi_frequency);
+
+/**
+ * @internal
+ * @brief Registers a callback to invoke after each runtime measure and
+ * update of the LSI frequency is completed.
+ *
+ * @param cb		Callback to invoke
+ * @return 0		Registration successful
+ * @return ENOMEM	Too many callbacks registered
+ *
+ * @note Callbacks are NEVER invoked if runtime LSI measurement is disabled
+ */
+int stm32wb0_register_lsi_update_callback(lsi_update_cb_t cb);
+#endif /* CONFIG_SOC_SERIES_STM32WB0X */
 
 #endif /* ZEPHYR_INCLUDE_DRIVERS_CLOCK_CONTROL_STM32_CLOCK_CONTROL_H_ */