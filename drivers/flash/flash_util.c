zephyrproject-rtos/zephyr

drivers: flash: provide a generic flash_copy() algorithm
Provide a generic flash_copy() algorithm that is capable of
copying from one flash device to another or within different
regions of the same flash device.

Additionally, provide a flash_region_crc() function that can
be used to compute the IEEE CRC32 of arbitrary regions of
flash memory, as long as CONFIG_CRC is enabled.

Additionally, provide a flash_regions_overlap() function to
report whether flash regions within the same device overlap.

Signed-off-by: Chris Friedt <cfriedt@tenstorrent.com>

drivers/flash/flash_util.c
@@ -6,10 +6,17 @@
 
 
 #include <errno.h>
+#include <limits.h>
+#include <stddef.h>
+#include <stdint.h>
+#include <string.h>
+/* FIXME: use k_off_t instead of off_t */
+#include <sys/types.h>
 
 #include <zephyr/drivers/flash.h>
 #include <zephyr/logging/log.h>
-#include <string.h>
+#include <zephyr/sys/crc.h>
+#include <zephyr/sys/math_extras.h>
 
 LOG_MODULE_REGISTER(flash, CONFIG_FLASH_LOG_LEVEL);
 
@@ -79,4 +86,112 @@ int z_impl_flash_flatten(const struct device *dev, off_t offset, size_t size)
 #else
 	return -ENOSYS;
 #endif
+}
+
+static bool off_add_overflow(off_t offset, off_t size, off_t *result)
+{
+	if (sizeof(off_t) == sizeof(uint32_t)) {
+		uint32_t end;
+
+		/* account for signedness of off_t due to lack of s32_add_overflow() */
+		if (u32_add_overflow((uint32_t)offset, (uint32_t)size, &end) ||
+		    (end > (UINT32_MAX >> 1))) {
+			return true;
+		}
+	} else if (sizeof(off_t) == sizeof(uint64_t)) {
+		uint64_t end;
+
+		/* account for signedness of off_t due to lack of s64_add_overflow() */
+		if (u64_add_overflow((uint64_t)offset, (uint64_t)size, &end) ||
+		    (end > (UINT64_MAX >> 1))) {
+			return true;
+		}
+	}
+
+	return false;
+}
+
+int flash_ranges_overlap(off_t a_start, off_t a_size, off_t b_start, off_t b_size)
+{
+	off_t a_end = 0;
+	off_t b_end = 0;
+
+	BUILD_ASSERT((sizeof(off_t) == sizeof(uint32_t)) || (sizeof(off_t) == sizeof(uint64_t)));
+
+	if ((a_start < 0) || (a_size < 0) || (b_start < 0) || (b_size < 0)) {
+		return -EINVAL;
+	}
+
+	if (off_add_overflow(a_start, a_size, &a_end) ||
+	    off_add_overflow(b_start, b_size, &b_end)) {
+		return -EINVAL;
+	}
+
+	return (a_start < b_end) && (a_end > b_start);
+}
+
+int flash_copy(const struct device *src_dev, off_t src_offset, const struct device *dst_dev,
+	       off_t dst_offset, off_t size, uint32_t *buf, size_t buf_size)
+{
+	int ret;
+	uint32_t write_size;
+
+	if ((src_offset < 0) || (dst_offset < 0) || (size < 0) || (buf == NULL) ||
+	    (buf_size == 0)) {
+		LOG_DBG("invalid argument");
+		return -EINVAL;
+	}
+
+	if ((src_dev == dst_dev) && (src_offset == dst_offset)) {
+		/* nothing to do */
+		return 0;
+	}
+
+	if ((src_dev == dst_dev) &&
+	    (flash_ranges_overlap(src_offset, size, dst_offset, size) != 0)) {
+		LOG_DBG("%s (%lx) and %s (%lx) overlap with size (%lx)", "source", (long)src_offset,
+			"destination", (long)dst_offset, (long)size);
+		return -EINVAL;
+	}
+
+	if (!device_is_ready(src_dev)) {
+		LOG_DBG("%s device not ready", "source");
+		return -ENODEV;
+	}
+
+	if (!device_is_ready(dst_dev)) {
+		LOG_DBG("%s device not ready", "destination");
+		return -ENODEV;
+	}
+
+	write_size = flash_get_write_block_size(dst_dev);
+	if ((buf_size < write_size) || ((buf_size % write_size) != 0)) {
+		LOG_DBG("buf size %zu is incompatible with write_size of %zu", buf_size,
+			write_size);
+		return -EINVAL;
+	}
+
+	for (uint32_t offs = 0, N = size, bytes_read = 0, bytes_left = N; offs < N;
+	     offs += bytes_read, bytes_left -= bytes_read) {
+
+		if (bytes_left < write_size) {
+			memset(buf, 0, write_size);
+		}
+		bytes_read = MIN(MAX(bytes_left, write_size), buf_size);
+		ret = flash_read(src_dev, src_offset + offs, buf, bytes_read);
+		if (ret < 0) {
+			LOG_DBG("%s failed at offset %lx: %d", "flash_read()",
+				(long)(src_offset + offs), ret);
+			return ret;
+		}
+
+		ret = flash_write(dst_dev, dst_offset + offs, buf, bytes_read);
+		if (ret < 0) {
+			LOG_DBG("%s failed at offset %lx: %d", "flash_write()",
+				(long)(src_offset + offs), ret);
+			return ret;
+		}
+	}
+
+	return 0;
 }

coreboot/zephyr-cros

(cherry picked from commit ced4e1623574ab4c06eacb99267a3d003c14bbeb)
Original-Signed-off-by: Chris Friedt <cfriedt@tenstorrent.com>
GitOrigin-RevId: ced4e1623574ab4c06eacb99267a3d003c14bbeb
Cr-Build-Id: 8734380568354140561
Cr-Build-Url: https://cr-buildbucket.appspot.com/build/8734380568354140561
Copybot-Job-Name: zephyr-main-copybot-downstream
Change-Id: I392b0cb36152c181f326b186cd2f2007f3483f56
Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/zephyr/+/5915901
Reviewed-by: Jeremy Bettis <jbettis@chromium.org>
Commit-Queue: Jeremy Bettis <jbettis@chromium.org>
Tested-by: Jeremy Bettis <jbettis@chromium.org>
Tested-by: ChromeOS Prod (Robot) <chromeos-ci-prod@chromeos-bot.iam.gserviceaccount.com>

drivers/flash/flash_util.c
@@ -6,10 +6,17 @@
 
 
 #include <errno.h>
+#include <limits.h>
+#include <stddef.h>
+#include <stdint.h>
+#include <string.h>
+/* FIXME: use k_off_t instead of off_t */
+#include <sys/types.h>
 
 #include <zephyr/drivers/flash.h>
+#include <zephyr/internal/syscall_handler.h>
 #include <zephyr/logging/log.h>
-#include <string.h>
+#include <zephyr/sys/math_extras.h>
 
 LOG_MODULE_REGISTER(flash, CONFIG_FLASH_LOG_LEVEL);
 
@@ -80,3 +87,116 @@ int z_impl_flash_flatten(const struct device *dev, off_t offset, size_t size)
 	return -ENOSYS;
 #endif
 }
+
+/* note: caller must first check for positivity (>=0) */
+static inline bool off_add_overflow(off_t offset, off_t size, off_t *result)
+{
+	BUILD_ASSERT((sizeof(off_t) == sizeof(uint32_t)) || (sizeof(off_t) == sizeof(uint64_t)));
+
+	if (sizeof(off_t) == sizeof(uint32_t)) {
+		uint32_t end;
+
+		/* account for signedness of off_t due to lack of s32_add_overflow() */
+		if (u32_add_overflow((uint32_t)offset, (uint32_t)size, &end) || (end > INT32_MAX)) {
+			return true;
+		}
+	} else if (sizeof(off_t) == sizeof(uint64_t)) {
+		uint64_t end;
+
+		/* account for signedness of off_t due to lack of s64_add_overflow() */
+		if (u64_add_overflow((uint64_t)offset, (uint64_t)size, &end) || (end > INT64_MAX)) {
+			return true;
+		}
+	}
+
+	return false;
+}
+
+/* note: caller must first check for overflow */
+static inline bool flash_ranges_overlap(off_t a_start, off_t a_size, off_t b_start, off_t b_size)
+{
+	off_t a_end = a_start + a_size;
+	off_t b_end = b_start + b_size;
+
+	return (a_start < b_end) && (a_end > b_start);
+}
+
+int z_impl_flash_copy(const struct device *src_dev, off_t src_offset, const struct device *dst_dev,
+		      off_t dst_offset, off_t size, uint8_t *buf, size_t buf_size)
+{
+	int ret;
+	off_t end;
+	size_t write_size;
+
+	if ((src_offset < 0) || (dst_offset < 0) || (size < 0) || (buf == NULL) ||
+	    (buf_size == 0) || off_add_overflow(src_offset, size, &end) ||
+	    off_add_overflow(dst_offset, size, &end)) {
+		LOG_DBG("invalid argument");
+		return -EINVAL;
+	}
+
+	if (src_dev == dst_dev) {
+		if (src_offset == dst_offset) {
+			return 0;
+		}
+
+		if (flash_ranges_overlap(src_offset, size, dst_offset, size) != 0) {
+			return -EINVAL;
+		}
+	}
+
+	if (!device_is_ready(src_dev)) {
+		LOG_DBG("%s device not ready", "src");
+		return -ENODEV;
+	}
+
+	if (!device_is_ready(dst_dev)) {
+		LOG_DBG("%s device not ready", "dst");
+		return -ENODEV;
+	}
+
+	write_size = flash_get_write_block_size(dst_dev);
+	if ((buf_size < write_size) || ((buf_size % write_size) != 0)) {
+		LOG_DBG("buf size %zu is incompatible with write_size of %zu", buf_size,
+			write_size);
+		return -EINVAL;
+	}
+
+	for (uint32_t offs = 0, N = size, bytes_read = 0, bytes_left = N; offs < N;
+	     offs += bytes_read, bytes_left -= bytes_read) {
+
+		if (bytes_left < write_size) {
+			const struct flash_driver_api *api =
+				(const struct flash_driver_api *)dst_dev->api;
+			const struct flash_parameters *params = api->get_parameters(dst_dev);
+
+			memset(buf, params->erase_value, write_size);
+		}
+		bytes_read = MIN(MAX(bytes_left, write_size), buf_size);
+		ret = flash_read(src_dev, src_offset + offs, buf, bytes_read);
+		if (ret < 0) {
+			LOG_DBG("%s() failed at offset %lx: %d", "flash_read",
+				(long)(src_offset + offs), ret);
+			return ret;
+		}
+
+		ret = flash_write(dst_dev, dst_offset + offs, buf, bytes_read);
+		if (ret < 0) {
+			LOG_DBG("%s() failed at offset %lx: %d", "flash_write",
+				(long)(src_offset + offs), ret);
+			return ret;
+		}
+	}
+
+	return 0;
+}
+
+#ifdef CONFIG_USERSPACE
+int z_vrfy_flash_copy(const struct device *src_dev, off_t src_offset, const struct device *dst_dev,
+		      off_t dst_offset, off_t size, uint8_t *buf, size_t buf_size)
+{
+	K_OOPS(K_SYSCALL_MEMORY_WRITE(buf, buf_size));
+	return z_impl_flash_copy(src_dev, src_offset, dst_dev, dst_offset, size, buf, buf_size);
+}
+#include <zephyr/syscalls/flash_copy_mrsh.c>
+#endif