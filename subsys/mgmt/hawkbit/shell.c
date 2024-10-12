dztang/zephyr

mgmt: hawkbit: also use workqueue for shell run
Also use a workqueue, when execution of
hawkBit is requested via shell.

Signed-off-by: Fin Maaß <f.maass@vogl-electronic.com>

subsys/mgmt/hawkbit/shell.c
@@ -19,13 +19,14 @@ static void cmd_run(const struct shell *sh, size_t argc, char **argv)
 	ARG_UNUSED(argv);
 
 	shell_fprintf(sh, SHELL_INFO, "Starting hawkBit run...\n");
-	switch (hawkbit_probe()) {
+	hawkbit_autohandler(false);
+
+	switch (hawkbit_autohandler_wait(__UINT32_MAX__, K_FOREVER)) {
 	case HAWKBIT_UNCONFIRMED_IMAGE:
 		shell_fprintf(
 			sh, SHELL_ERROR,
 			"Image is unconfirmed."
 			"Rebooting to revert back to previous confirmed image\n");
-		hawkbit_reboot();
 		break;
 
 	case HAWKBIT_CANCEL_UPDATE:
@@ -43,7 +44,6 @@ static void cmd_run(const struct shell *sh, size_t argc, char **argv)
 
 	case HAWKBIT_UPDATE_INSTALLED:
 		shell_fprintf(sh, SHELL_INFO, "Update Installed\n");
-		hawkbit_reboot();
 		break;
 
 	case HAWKBIT_DOWNLOAD_ERROR:

coreboot/zephyr-cros

(cherry picked from commit 4655ef317ec1f38616c78575f7bb34fb27e9f5d0)
Original-Signed-off-by: Fin Maaß <f.maass@vogl-electronic.com>
GitOrigin-RevId: 4655ef317ec1f38616c78575f7bb34fb27e9f5d0
Cr-Build-Id: 8734380568354140561
Cr-Build-Url: https://cr-buildbucket.appspot.com/build/8734380568354140561
Copybot-Job-Name: zephyr-main-copybot-downstream
Change-Id: I78f5599365369f6ccc2e1f50e8c33176c90990dd
Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/zephyr/+/5923637
Reviewed-by: Jeremy Bettis <jbettis@chromium.org>
Commit-Queue: Jeremy Bettis <jbettis@chromium.org>
Tested-by: Jeremy Bettis <jbettis@chromium.org>
Tested-by: ChromeOS Prod (Robot) <chromeos-ci-prod@chromeos-bot.iam.gserviceaccount.com>

subsys/mgmt/hawkbit/shell.c
@@ -20,7 +20,9 @@ static void cmd_run(const struct shell *sh, size_t argc, char **argv)
 
 
 	shell_info(sh, "Starting hawkBit run...");
-	switch (hawkbit_probe()) {
+	hawkbit_autohandler(false);
+
+	switch (hawkbit_autohandler_wait(UINT32_MAX, K_FOREVER)) {
 	case HAWKBIT_UNCONFIRMED_IMAGE:
 		shell_error(sh, "Image is unconfirmed."
 				"Rebooting to revert back to previous confirmed image");
@@ -40,7 +42,6 @@ static void cmd_run(const struct shell *sh, size_t argc, char **argv)
 
 	case HAWKBIT_UPDATE_INSTALLED:
 		shell_info(sh, "Update installed");
-		hawkbit_reboot();
 		break;
 
 	case HAWKBIT_DOWNLOAD_ERROR: