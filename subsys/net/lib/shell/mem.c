zephyrproject-rtos/zephyr

net: shell: mem: Add net_pkt allocation statistics printout
The "net mem" command can show extra information for net_pkt
allocations.

Signed-off-by: Jukka Rissanen <jukka.rissanen@nordicsemi.no>

subsys/net/lib/shell/mem.c
@@ -153,6 +153,28 @@ static int cmd_net_mem(const struct shell *sh, size_t argc, char *argv[])
 			PR("No external memory pools found.\n");
 		}
 	}
+
+#if defined(CONFIG_NET_PKT_ALLOC_STATS)
+	PR("\n");
+	PR("Slab\t\tStatus\tAllocs\tAvg size\tAvg time (usec)\n");
+
+	STRUCT_SECTION_FOREACH(net_pkt_alloc_stats_slab, stats) {
+		if (stats->ok.count) {
+			PR("%p\tOK  \t%lu\t%llu\t\t%llu\n", stats->slab, stats->ok.count,
+			   stats->ok.alloc_sum / (uint64_t)stats->ok.count,
+			   k_ticks_to_us_floor64(stats->ok.time_sum /
+						 (uint64_t)stats->ok.count));
+		}
+
+		if (stats->fail.count) {
+			PR("%p\tFAIL\t%lu\t%llu\t\t%llu\n", stats->slab, stats->fail.count,
+			   stats->fail.alloc_sum / (uint64_t)stats->fail.count,
+			   k_ticks_to_us_floor64(stats->fail.time_sum /
+						 (uint64_t)stats->fail.count));
+		}
+	}
+#endif /* CONFIG_NET_PKT_ALLOC_STATS */
+
 #else
 	PR_INFO("Set %s to enable %s support.\n",
 		"CONFIG_NET_OFFLOAD or CONFIG_NET_NATIVE", "memory usage");

coreboot/zephyr-cros

(cherry picked from commit d3fbc366eaa843a3fd7d1b72d6c8f9c224d22296)
Original-Signed-off-by: Jukka Rissanen <jukka.rissanen@nordicsemi.no>
GitOrigin-RevId: d3fbc366eaa843a3fd7d1b72d6c8f9c224d22296
Cr-Build-Id: 8732749822481685601
Cr-Build-Url: https://cr-buildbucket.appspot.com/build/8732749822481685601
Copybot-Job-Name: zephyr-main-copybot-downstream
Change-Id: I931bb109f4ab9ac90e0b8a0b7314ca7facafb2f8
Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/zephyr/+/5975858
Reviewed-by: Ting Shen <phoenixshen@chromium.org>
Commit-Queue: Ting Shen <phoenixshen@chromium.org>
Tested-by: ChromeOS Prod (Robot) <chromeos-ci-prod@chromeos-bot.iam.gserviceaccount.com>

subsys/net/lib/shell/mem.c
@@ -153,6 +153,28 @@ static int cmd_net_mem(const struct shell *sh, size_t argc, char *argv[])
 			PR("No external memory pools found.\n");
 		}
 	}
+
+#if defined(CONFIG_NET_PKT_ALLOC_STATS)
+	PR("\n");
+	PR("Slab\t\tStatus\tAllocs\tAvg size\tAvg time (usec)\n");
+
+	STRUCT_SECTION_FOREACH(net_pkt_alloc_stats_slab, stats) {
+		if (stats->ok.count) {
+			PR("%p\tOK  \t%u\t%llu\t\t%llu\n", stats->slab, stats->ok.count,
+			   stats->ok.alloc_sum / (uint64_t)stats->ok.count,
+			   k_cyc_to_us_ceil64(stats->ok.time_sum /
+					      (uint64_t)stats->ok.count));
+		}
+
+		if (stats->fail.count) {
+			PR("%p\tFAIL\t%u\t%llu\t\t%llu\n", stats->slab, stats->fail.count,
+			   stats->fail.alloc_sum / (uint64_t)stats->fail.count,
+			   k_cyc_to_us_ceil64(stats->fail.time_sum /
+					      (uint64_t)stats->fail.count));
+		}
+	}
+#endif /* CONFIG_NET_PKT_ALLOC_STATS */
+
 #else
 	PR_INFO("Set %s to enable %s support.\n",
 		"CONFIG_NET_OFFLOAD or CONFIG_NET_NATIVE", "memory usage");