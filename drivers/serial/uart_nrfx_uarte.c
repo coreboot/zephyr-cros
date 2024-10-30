silverlee35/z_example

drivers: serial: nrfx_uarte: Add lock to uart_rx_disable
Add lock to fix race when uart_rx_disable is interrupted by RXTO
event which lead to driver state corruption.

Signed-off-by: Krzysztof Chruściński <krzysztof.chruscinski@nordicsemi.no>

drivers/serial/uart_nrfx_uarte.c
@@ -1111,20 +1111,26 @@ static int uarte_nrfx_rx_disable(const struct device *dev)
 	struct uarte_nrfx_data *data = dev->data;
 	struct uarte_async_rx *rdata = &data->async->rx;
 	NRF_UARTE_Type *uarte = get_uarte_instance(dev);
+	int key;
 
 	if (rdata->buf == NULL) {
 		return -EFAULT;
 	}
+
+	k_timer_stop(&rdata->timer);
+
+	key = irq_lock();
+
 	if (rdata->next_buf != NULL) {
 		nrf_uarte_shorts_disable(uarte, NRF_UARTE_SHORT_ENDRX_STARTRX);
 		nrf_uarte_event_clear(uarte, NRF_UARTE_EVENT_RXSTARTED);
 	}
 
-	k_timer_stop(&rdata->timer);
 	rdata->enabled = false;
 	rdata->discard_fifo = true;
 
 	nrf_uarte_task_trigger(uarte, NRF_UARTE_TASK_STOPRX);
+	irq_unlock(key);
 
 	return 0;
 }

coreboot/zephyr-cros

(cherry picked from commit 317de0c6dea11466f7465addeeb7d04076f32f4f)
Original-Signed-off-by: Krzysztof Chruściński <krzysztof.chruscinski@nordicsemi.no>
GitOrigin-RevId: 317de0c6dea11466f7465addeeb7d04076f32f4f
Cr-Build-Id: 8732749822481685601
Cr-Build-Url: https://cr-buildbucket.appspot.com/build/8732749822481685601
Copybot-Job-Name: zephyr-main-copybot-downstream
Change-Id: I66ff5a601068511fa0b93fcfe85d03f47c08a614
Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/zephyr/+/5975793
Commit-Queue: Ting Shen <phoenixshen@chromium.org>
Reviewed-by: Ting Shen <phoenixshen@chromium.org>
Tested-by: ChromeOS Prod (Robot) <chromeos-ci-prod@chromeos-bot.iam.gserviceaccount.com>

drivers/serial/uart_nrfx_uarte.c
@@ -1099,20 +1099,26 @@ static int uarte_nrfx_rx_disable(const struct device *dev)
 	struct uarte_nrfx_data *data = dev->data;
 	struct uarte_async_rx *async_rx = &data->async->rx;
 	NRF_UARTE_Type *uarte = get_uarte_instance(dev);
+	int key;
 
 	if (async_rx->buf == NULL) {
 		return -EFAULT;
 	}
+
+	k_timer_stop(&async_rx->timer);
+
+	key = irq_lock();
+
 	if (async_rx->next_buf != NULL) {
 		nrf_uarte_shorts_disable(uarte, NRF_UARTE_SHORT_ENDRX_STARTRX);
 		nrf_uarte_event_clear(uarte, NRF_UARTE_EVENT_RXSTARTED);
 	}
 
-	k_timer_stop(&async_rx->timer);
 	async_rx->enabled = false;
 	async_rx->discard_fifo = true;
 
 	nrf_uarte_task_trigger(uarte, NRF_UARTE_TASK_STOPRX);
+	irq_unlock(key);
 
 	return 0;
 }